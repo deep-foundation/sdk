90955e9e9cfc5d44eee50046bd13dc97dcb1655a
Roman <technopriest666@gmail.com>
2022-12-12 22:58:23 +0300

    +android

---
+++ android/app/src/main/java/deep/case/app/MainActivity.java
@@ -0,0 +1,5 @@
+package deep.case.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,71 @@
-import React, { useMemo, useState } from 'react';
-import { ApolloClientTokenizedProvider } from '@deep-foundation/react-hasura/apollo-client-tokenized-provider';
-import { TokenProvider, useTokenController } from '@deep-foundation/deeplinks/imports/react-token';
-import { useQuery, useSubscription, gql } from '@apollo/client';
-import { LocalStoreProvider, useLocalStore } from '@deep-foundation/store/local';
-import { MinilinksLink, MinilinksResult, useMinilinksConstruct } from '@deep-foundation/deeplinks/imports/minilinks';
-import { ChakraProvider } from '@chakra-ui/react';
+import React, { useMemo, useState, useEffect } from "react";
+import { Contacts } from "@capacitor-community/contacts";
+import {
+  DeepProvider,
+  useDeep,
+} from "@deep-foundation/deeplinks/imports/client";
+import { Provider } from "../imports/provider";
+
+function Content() {
+  const deep = useDeep();
+
+  async function getContact() {
+    await Contacts.getPermissions();
+    const contacts = await Contacts.getContacts();
+    console.log(JSON.stringify(contacts));
+  }
+
+  const authUser = async () => {
+    await deep.guest();
+    const { linkId, token, error } = await deep.login({
+      linkId: await deep.id("deep", 'admin')
+    })
+    console.log(linkId, token, error);
+  };
+
+  async function addContactDeep() {
+    await addLink("contact", "", 362);
+  }
+
+  const addLink = async (
+    containName: string,
+    value: string,
+    from_id: number
+  ) => {
+    const new_list = await deep.insert({
+      type_id: 1,
+      in: {
+        data: {
+          type_id: 3,
+          from_id: from_id, // 362
+          string: { data: { value: containName } },
+        },
+      },
+    });
+
+    console.log(JSON.stringify(new_list));
+  };
+
+  return (
+    <>
+      <button onClick={() => authUser()}>Auth User</button>
+      <button onClick={getContact}>Get Contact</button>
+      <button onClick={() => addContactDeep()}>add Contact</button>
+    </>
+  );
+}
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function Index() {
-  return (<>
-    <ChakraProvider>
-      <h1>Deep.Foundation nextjs examples</h1>
-      <div><a href="/all">all subscribe</a></div>
-      <div><a href="/messanger">messanger</a></div>
-    </ChakraProvider>
-  </>);
+  return (
+    <>
+      <ChakraProvider>
+        <Provider>
+          <DeepProvider>
+            <Content />
+          </DeepProvider>
+        </Provider>
+      </ChakraProvider>
+    </>
+  );
 }
