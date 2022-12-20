360dd2bade668c9f220d8987477db62fc0ba906f
FreePhoenix888 <freephoenix888@gmail.com>
2022-12-20 21:00:33 +0600

    Add capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.deepfoundation.deep',
+  appName: 'Deep',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
