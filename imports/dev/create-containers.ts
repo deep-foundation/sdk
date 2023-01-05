2e33024a3190c8db837c31e994a2194c21ba3827
Roman <technopriest666@gmail.com>
2023-01-05 23:24:04 +0300

    network mvp

---
+++ imports/dev/create-containers.ts
@@ -0,0 +1,22 @@
+import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
+import { PACKAGE_NAME } from "./initialize-package";
+
+export default async function createContainers(deep: DeepClient, containers:string[]) {
+  const containTypeLinkId = await deep.id("@deep-foundation/core", "Contain")
+  const containerTypeLinkId = await deep.id(PACKAGE_NAME, "Container")
+
+  const { data } = await deep.insert(containers.map((container) => ({
+    type_id: containerTypeLinkId,
+    string: { data: { value: container } },
+    in: {
+      data: [{
+        type_id: containTypeLinkId,
+        from_id: deep.linkId,
+        string: { data: { value: container } },
+      }]
+    }
+  })))
+  const containerIds = data.map((item) => item.id);
+  console.log(containerIds);
+  return containerIds;
+}
---
+++ pages/network.tsx
@@ -0,0 +1,108 @@
+import React, { useCallback, useEffect } from 'react';
+import { Network as CapacitorNetwork } from "@capacitor/network"
+import { useLocalStore } from '@deep-foundation/store/local';
+import { DeepProvider, useDeep, DeepClient } from '@deep-foundation/deeplinks/imports/client';
+import { Provider } from '../imports/provider';
+import { Button, ChakraProvider, Stack, Text } from '@chakra-ui/react';
+
+import initializePackage, { PACKAGE_NAME } from "../imports/network/initialize-package";
+import saveNetworkStatus from '../imports/network/save-network-status';
+
+
+
+function Page() {
+  const deep = useDeep();
+  const [connections, setConnections] = useLocalStore("ConnectionTypes", []);
+
+  useEffect(() => {
+    const uploadConnections = async (connections) => {
+      const containTypeLinkId = await deep.id("@deep-foundation/core", "Contain");
+      const connectionTypeLinkId = await deep.id(PACKAGE_NAME, "Connection");
+      const connectionTypeTypeLinkId = await deep.id(PACKAGE_NAME, "ConnectionType");
+      const connectedTypeLinkId = await deep.id(PACKAGE_NAME, "Connected");
+      const timestampTypeLinkId = await deep.id(PACKAGE_NAME, "Timestamp");
+      const customContainerTypeLinkId = await deep.id(deep.linkId, "Network");
+
+      await deep.insert(connections.map((connection) => ({
+        type_id: connectionTypeLinkId,
+        in: {
+          data: [{
+            type_id: containTypeLinkId,
+            from_id: customContainerTypeLinkId,
+          }]
+        },
+        out: {
+          data: [
+            {
+              type_id: containTypeLinkId,
+              to: {
+                data: {
+                  type_id: connectionTypeTypeLinkId,
+                  string: { data: { value: connection.connectionType } },
+                }
+              }
+            },
+            {
+              type_id: containTypeLinkId,
+              to: {
+                data: {
+                  type_id: connectedTypeLinkId,
+                  boolean: { data: { value: connection.connected } },
+                }
+              }
+            },
+            {
+              type_id: containTypeLinkId,
+              to: {
+                data: {
+                  type_id: timestampTypeLinkId,
+                  string: { data: { value: new Date().toLocaleDateString() } },
+                }
+              }
+            }]
+        }
+      })));
+    }
+    if (connections.length > 0) {
+      uploadConnections(connections);
+      setConnections([]);
+    }
+  }, [connections])
+
+  async function subscribeToNetworkStatus() {
+    CapacitorNetwork.addListener('networkStatusChange', async (connection) => {
+      console.log({ connection });
+      setConnections([...connections, connection]);
+    })
+  }
+
+  return <Stack>
+    <Button onClick={async () => {
+      await deep.guest();
+      await deep.login({ linkId: await deep.id("deep", "admin") });
+    }}>
+      <Text>Login as admin</Text>
+    </Button>
+    <Button onClick={async () => { await initializePackage(deep) }}>
+      <Text>Initialize package</Text>
+    </Button>
+    <Button onClick={async () => { await subscribeToNetworkStatus() }}>
+      <Text>Listen to changes</Text>
+    </Button>
+    <Button onClick={async () => await saveNetworkStatus(deep)}>
+      <Text>Save Current Status</Text>
+    </Button>
+  </Stack>
+}
+
+export default function Network() {
+  return (
+    <ChakraProvider>
+      <Provider>
+        <DeepProvider>
+          <Page />
+        </DeepProvider>
+      </Provider>
+    </ChakraProvider>
+  );
+}
