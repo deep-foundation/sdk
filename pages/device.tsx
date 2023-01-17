052817fecdb561bc17567369529f33ef0a397606
FreePhoenix888 <freephoenix888@gmail.com>
2023-01-18 01:28:58 +0600

    Update draft

--- pages/device.tsx
+++ pages/device.tsx
@@ -1,8 +1,14 @@
-
 import React, { useCallback } from 'react';
 import { TokenProvider } from '@deep-foundation/deeplinks/imports/react-token';
-import { LocalStoreProvider, useLocalStore } from '@deep-foundation/store/local';
-import { DeepProvider, useDeep, useDeepSubscription } from '@deep-foundation/deeplinks/imports/client';
+import {
+  LocalStoreProvider,
+  useLocalStore,
+} from '@deep-foundation/store/local';
+import {
+  DeepProvider,
+  useDeep,
+  useDeepSubscription,
+} from '@deep-foundation/deeplinks/imports/client';
 
 import { Button, ChakraProvider, Stack, Text } from '@chakra-ui/react';
 import { saveGeneralInfo } from '../imports/device/save-general-info';
@@ -13,58 +19,64 @@ import { getLanguageId as saveLanguageId } from '../imports/device/save-language
 import { getLanguageTag as saveLanguageTag } from '../imports/device/save-language-tag';
 import { Provider } from '../imports/provider';
 
-
 function Page() {
-
   const deep = useDeep();
-  const [deviceLinkId, setDeviceLinkId] = useLocalStore("deviceLinkId", undefined);
+  const [deviceLinkId, setDeviceLinkId] = useLocalStore(
+    'deviceLinkId',
+    undefined
+  );
 
-  return <Stack>
-    <Button onClick={async () => {
-      await deep.guest();
-      await deep.login({
-        linkId: await deep.id("deep", "admin")
-      });
-    }}>Login as admin</Button>
-        <Text>{deviceLinkId}</Text>
-    <Button onClick={async () => {
-      console.log({deviceLinkId});
-      await initializePackage(deep);
-      if(!deviceLinkId) {
-        const deviceTypeLinkId = await deep.id(PACKAGE_NAME, "Device");
-        const {data: [{id: newDeviceLinkId}]} = await deep.insert({
-          type_id: deviceTypeLinkId
-        })
-        console.log({newDeviceLinkId});
-        setDeviceLinkId(newDeviceLinkId);
-      }
-      console.log({deviceLinkId});
-      
-    }}>Initialize package</Button>
-    
-    <Button onClick={useCallback(() => {
-      saveGeneralInfo(deep, deviceLinkId)
-    }, [deep, deviceLinkId])}>Save general info</Button>
-    <Button onClick={useCallback(() => {
-      saveBatteryInfo(deep, deviceLinkId)
-    }, [deep, deviceLinkId])}>Save battery info</Button>
-    <Button onClick={useCallback(() => {
-      saveLanguageId(deep, deviceLinkId)
-    }, [deep, deviceLinkId])}>Save language id</Button>
-    <Button onClick={useCallback(() => {
-      saveLanguageTag(deep, deviceLinkId)
-    }, [deep, deviceLinkId])}>Save language tag</Button>
-  </Stack>;
+  return (
+    <Stack>
+      <Text>{deviceLinkId}</Text>
+      <Button
+        onClick={useCallback(() => {
+          saveGeneralInfo(deep, deviceLinkId);
+        }, [deep, deviceLinkId])}
+      >
+        Save general info
+      </Button>
+      <Button
+        onClick={useCallback(() => {
+          saveBatteryInfo(deep, deviceLinkId);
+        }, [deep, deviceLinkId])}
+      >
+        Save battery info
+      </Button>
+      <Button
+        onClick={useCallback(() => {
+          saveLanguageId(deep, deviceLinkId);
+        }, [deep, deviceLinkId])}
+      >
+        Save language id
+      </Button>
+      <Button
+        onClick={useCallback(() => {
+          saveLanguageTag(deep, deviceLinkId);
+        }, [deep, deviceLinkId])}
+      >
+        Save language tag
+      </Button>
+      <Button
+        onClick={useCallback(() => {
+          console.log(deep);
+          
+        }, [deep, deviceLinkId])}
+      >
+        Aaaaaaaaaa
+      </Button>
+    </Stack>
+  );
 }
 
 export default function Device() {
   return (
-		<ChakraProvider>
-			<Provider>
-				<DeepProvider>
-					<Page />
-				</DeepProvider>
-			</Provider>
-		</ChakraProvider>
-	);
+    <ChakraProvider>
+      <Provider>
+        <DeepProvider>
+          <Page />
+        </DeepProvider>
+      </Provider>
+    </ChakraProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,129 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { ApolloClientTokenizedProvider } from '@deep-foundation/react-hasura/apollo-client-tokenized-provider';
-import { TokenProvider, useTokenController } from '@deep-foundation/deeplinks/imports/react-token';
+import {
+  TokenProvider,
+  useTokenController,
+} from '@deep-foundation/deeplinks/imports/react-token';
 import { useQuery, useSubscription, gql } from '@apollo/client';
-import { LocalStoreProvider, useLocalStore } from '@deep-foundation/store/local';
-import { MinilinksLink, MinilinksResult, useMinilinksConstruct } from '@deep-foundation/deeplinks/imports/minilinks';
+import {
+  LocalStoreProvider,
+  useLocalStore,
+} from '@deep-foundation/store/local';
+import {
+  MinilinksLink,
+  MinilinksResult,
+  useMinilinksConstruct,
+} from '@deep-foundation/deeplinks/imports/minilinks';
 import { ChakraProvider, Text } from '@chakra-ui/react';
 import { Provider } from '../imports/provider';
-import { DeepProvider, useDeep } from '@deep-foundation/deeplinks/imports/client';
+import {
+  DeepProvider,
+  useDeep,
+} from '@deep-foundation/deeplinks/imports/client';
 import Link from 'next/link';
 import { initializePackage } from '../imports/device/initialize-package';
 import { PACKAGE_NAME as DEVICE_PACKAGE_NAME } from '../imports/device/package-name';
 
-export default function Index() {
-
+function Page() {
   const deep = useDeep();
-  const [deviceLinkId, setDeviceLinkId] = useLocalStore("deviceLinkId", undefined);
 
+  const [deviceLinkId, setDeviceLinkId] = useLocalStore(
+    'deviceLinkId',
+    undefined
+  );
 
   useEffect(() => {
     async function loginAsAdminInDeep() {
+      if (!deep) {
+        return;
+      }
+
       await deep.guest();
       await deep.login({
-        linkId: await deep.id("deep", "admin")
+        linkId: await deep.id('deep', 'admin'),
       });
     }
     loginAsAdminInDeep();
   }, []);
 
   useEffect(() => {
+    if (!deep) {
+      return;
+    }
     async function initializeDevice() {
-      const isDevicePackageInstalled = await deep.select({
+      const devicePackageSelectResponse = await deep.select({
         type_id: {
-          _id: ["@deep-foundation/core", "Contain"],
+          _id: ['@deep-foundation/core', 'Contain'],
         },
         from_id: deep.linkId,
         to: {
           type_id: {
-            _id: ["@deep-foundation/core", "Package"]
+            _id: ['@deep-foundation/core', 'Package'],
           },
           string: {
-            value: DEVICE_PACKAGE_NAME
-          }
-        }
+            value: DEVICE_PACKAGE_NAME,
+          },
+        },
       });
-      if(!isDevicePackageInstalled) {
+      const isDevicePackageInstalled =
+        devicePackageSelectResponse.data.length > 0;
+      if (!isDevicePackageInstalled) {
         await initializePackage(deep);
       }
       if (!deviceLinkId) {
-        const deviceTypeLinkId = await deep.id(DEVICE_PACKAGE_NAME, "Device");
-        const { data: [{ id: newDeviceLinkId }] } = await deep.insert({
-          type_id: deviceTypeLinkId
-        })
+        const deviceTypeLinkId = await deep.id(DEVICE_PACKAGE_NAME, 'Device');
+        const containTypeLinkId = await deep.id(
+          '@deep-foundation/core',
+          'Contain'
+        );
+        const {
+          data: [{ id: newDeviceLinkId }],
+        } = await deep.insert({
+          type_id: deviceTypeLinkId,
+          in: {
+            data: [
+              {
+                type_id: containTypeLinkId,
+                from_id: deep.linkId,
+              },
+            ],
+          },
+        });
         console.log({ newDeviceLinkId });
         setDeviceLinkId(newDeviceLinkId);
       }
     }
 
     initializeDevice();
-  }, [])
+  }, []);
 
-  return (<>
-    <ChakraProvider>
-      <Provider>
-        <DeepProvider>
-          <h1>Deep.Foundation sdk examples</h1>
-          <Text>Device Link Id: {deviceLinkId}</Text>
-          <div><Link href="/all">all subscribe</Link></div>
-          <div><Link href="/messanger">messanger</Link></div>
-          <div><Link href="/device">device</Link></div>
-        </DeepProvider>
-      </Provider>
-    </ChakraProvider>
-  </>);
+  return (
+    <>
+      <h1>Deep.Foundation sdk examples</h1>
+      <Text>Device Link Id: {deviceLinkId}</Text>
+      <div>
+        <Link href="/all">all subscribe</Link>
+      </div>
+      <div>
+        <Link href="/messanger">messanger</Link>
+      </div>
+      <div>
+        <Link href="/device">device</Link>
+      </div>
+    </>
+  );
+}
+
+export default function Index() {
+  return (
+    <>
+      <ChakraProvider>
+        <Provider>
+          <DeepProvider>
+            <Page />
+          </DeepProvider>
+        </Provider>
+      </ChakraProvider>
+    </>
+  );
 }
