c52263cbb67dfbe4e1d60c3f723b1da11d442e57
FreePhoenix888 <freephoenix888@gmail.com>
2023-01-18 12:45:54 +0600

    Login as guest then login as admin and only then initialize the device package and a device link

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,11 +21,13 @@ import {
   useDeep,
 } from '@deep-foundation/deeplinks/imports/client';
 import Link from 'next/link';
-import { initializePackage } from '../imports/device/initialize-package';
+import { initializePackage as initializeDevicePackage } from '../imports/device/initialize-package';
 import { PACKAGE_NAME as DEVICE_PACKAGE_NAME } from '../imports/device/package-name';
 
 function Page() {
   const deep = useDeep();
+  console.log('rerender');
+  console.log({ deep });
 
   const [deviceLinkId, setDeviceLinkId] = useLocalStore(
     'deviceLinkId',
@@ -33,73 +35,90 @@ function Page() {
   );
 
   useEffect(() => {
-    async function loginAsAdminInDeep() {
-      if (!deep) {
-        return;
-      }
+    console.log('Login as guest');
 
-      await deep.guest();
-      await deep.login({
-        linkId: await deep.id('deep', 'admin'),
-      });
-    }
-    loginAsAdminInDeep();
+    deep.guest();
   }, []);
 
   useEffect(() => {
-    if (!deep) {
+    new Promise(async () => {
+      if (deep.linkId != 0) {
+        const adminLinkId = await deep.id('deep', 'admin');
+        if (deep.linkId != adminLinkId) {
+          console.log('Login as admin');
+
+          await deep.login({
+            linkId: adminLinkId,
+          });
+        }
+      }
+    });
+  }, [deep]);
+
+  useEffect(() => {
+    if(deep.linkId == 0) {
       return;
     }
-    async function initializeDevice() {
-      const devicePackageSelectResponse = await deep.select({
-        type_id: {
-          _id: ['@deep-foundation/core', 'Contain'],
-        },
-        from_id: deep.linkId,
-        to: {
-          type_id: {
-            _id: ['@deep-foundation/core', 'Package'],
-          },
-          string: {
-            value: DEVICE_PACKAGE_NAME,
-          },
-        },
-      });
-      const isDevicePackageInstalled =
-        devicePackageSelectResponse.data.length > 0;
-      if (!isDevicePackageInstalled) {
-        await initializePackage(deep);
+    new Promise(async () => {
+      const adminLinkId = await deep.id('deep', 'admin');
+      if (deep.linkId != adminLinkId) {
+        return;
       }
-      if (!deviceLinkId) {
-        const deviceTypeLinkId = await deep.id(DEVICE_PACKAGE_NAME, 'Device');
-        const containTypeLinkId = await deep.id(
-          '@deep-foundation/core',
-          'Contain'
-        );
-        const {
-          data: [{ id: newDeviceLinkId }],
-        } = await deep.insert({
-          type_id: deviceTypeLinkId,
-          in: {
-            data: [
-              {
-                type_id: containTypeLinkId,
-                from_id: deep.linkId,
-              },
-            ],
+
+      const getIsDevicePackageInstalled = async() => {
+        const devicePackageSelectResponse = await deep.select({
+          type_id: {
+            _id: ['@deep-foundation/core', 'Contain'],
+          },
+          from_id: deep.linkId,
+          to: {
+            type_id: {
+              _id: ['@deep-foundation/core', 'Package'],
+            },
+            string: {
+              value: DEVICE_PACKAGE_NAME,
+            },
           },
         });
-        console.log({ newDeviceLinkId });
-        setDeviceLinkId(newDeviceLinkId);
+        const isDevicePackageInstalled =
+          devicePackageSelectResponse.data.length > 0;
+        return isDevicePackageInstalled;
       }
-    }
 
-    initializeDevice();
-  }, []);
+      if (!getIsDevicePackageInstalled()) {
+        await initializeDevicePackage(deep);
+      }
+      if (!deviceLinkId) {
+        const initializeDeviceLink = async () => {
+          const deviceTypeLinkId = await deep.id(DEVICE_PACKAGE_NAME, 'Device');
+          const containTypeLinkId = await deep.id(
+            '@deep-foundation/core',
+            'Contain'
+          );
+          const {
+            data: [{ id: newDeviceLinkId }],
+          } = await deep.insert({
+            type_id: deviceTypeLinkId,
+            in: {
+              data: [
+                {
+                  type_id: containTypeLinkId,
+                  from_id: deep.linkId,
+                },
+              ],
+            },
+          });
+          setDeviceLinkId(newDeviceLinkId);
+        };
+        initializeDeviceLink();
+      }
+    });
+  }, [deep]);
 
   return (
     <>
       <h1>Deep.Foundation sdk examples</h1>
+      <Text>Authentication Link Id: {deep.linkId}</Text>
       <Text>Device Link Id: {deviceLinkId}</Text>
       <div>
         <Link href="/all">all subscribe</Link>
