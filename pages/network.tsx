2d35e91e3fa6aaac0408e1832667adca04c0c377
Roman <technopriest666@gmail.com>
2023-01-20 23:59:40 +0300

    cleanup

--- pages/network.tsx
+++ pages/network.tsx
@@ -54,55 +54,6 @@ function Page() {
       }
     }
 
-    const uploadConnections = async (connections) => {
-      const containTypeLinkId = await deep.id("@deep-foundation/core", "Contain");
-      const connectionTypeLinkId = await deep.id(PACKAGE_NAME, "Connection");
-      const connectionTypeTypeLinkId = await deep.id(PACKAGE_NAME, "ConnectionType");
-      const connectedTypeLinkId = await deep.id(PACKAGE_NAME, "Connected");
-      const timestampTypeLinkId = await deep.id(PACKAGE_NAME, "Timestamp");
-      const networkLinkId = await deep.id(deep.linkId, "Network");
-
-      await deep.insert(connections.map((connection) => ({
-        type_id: connectionTypeLinkId,
-        in: {
-          data: [{
-            type_id: containTypeLinkId,
-            from_id: networkLinkId,
-          }]
-        },
-        out: {
-          data: [
-            {
-              type_id: containTypeLinkId,
-              to: {
-                data: {
-                  type_id: connectionTypeTypeLinkId,
-                  string: { data: { value: connection.connectionType } },
-                }
-              }
-            },
-            {
-              type_id: containTypeLinkId,
-              to: {
-                data: {
-                  type_id: connectedTypeLinkId,
-                  string: { data: { value: connection.connected ? "connected" : "disconnected" } },
-                }
-              }
-            },
-            {
-              type_id: containTypeLinkId,
-              to: {
-                data: {
-                  type_id: timestampTypeLinkId,
-                  string: { data: { value: new Date() } },
-                }
-              }
-            }]
-        }
-      })));
-    }
-
     if (connections.length > 0) {
       updateNetworkStatus(connections);
       setConnections([]);
