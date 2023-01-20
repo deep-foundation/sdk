87f50382b154545cd2eb32f15f911135e27f4d94
FreePhoenix888 <freephoenix888@gmail.com>
2023-01-20 13:53:15 +0000

    Add comments

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -6,10 +6,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    // For firebase cloud messaging (push notifications)
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
          NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
     }
 
+    // For firebase cloud messaging (push notifications)
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
     }
