2354ed38dbd56233aea0ba5a8e586bd25dc43d48
FreePhoenix888 <freephoenix888@gmail.com>
2023-01-20 12:59:35 +0000

    Initialize deep

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging/sw";
 import { onBackgroundMessage } from "firebase/messaging/sw";
+const { generateApolloClient } = require('@deep-foundation/hasura/client');
 import dotenv from ('dotenv');
 dotenv.config();
 
@@ -22,6 +23,22 @@ const firebaseApp = initializeApp(firebaseConfig);
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = getMessaging(firebaseApp);
+
+const apolloClient = generateApolloClient({
+  path: process.env.NEXT_PUBLIC_GQL_PATH || '', // <<= HERE PATH TO UPDATE
+  ssl: !!~process.env.NEXT_PUBLIC_GQL_PATH.indexOf('localhost')
+    ? false
+    : true,
+  // admin token in prealpha deep secret key
+  // token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsibGluayJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJsaW5rIiwieC1oYXN1cmEtdXNlci1pZCI6IjI2MiJ9LCJpYXQiOjE2NTYxMzYyMTl9.dmyWwtQu9GLdS7ClSLxcXgQiKxmaG-JPDjQVxRXOpxs',
+});
+const unloginedDeep = new DeepClient({ apolloClient });
+const guest = await unloginedDeep.guest();
+const guestDeep = new DeepClient({ deep: unloginedDeep, ...guest });
+const admin = await guestDeep.login({
+  linkId: await guestDeep.id('deep', 'admin'),
+});
+const deep = new DeepClient({ deep: guestDeep, ...admin });
 
 onBackgroundMessage(messaging, (payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
