27ad39abfad20e079089d166debcb9b89cf2d2a3
FreePhoenix888 <freephoenix888@gmail.com>
2023-01-21 07:55:09 +0000

    Add draft which listens not executed alerts and executes them

--- pages/dialog.tsx
+++ pages/dialog.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { TokenProvider } from '@deep-foundation/deeplinks/imports/react-token';
 import {
   LocalStoreProvider,
@@ -13,11 +13,12 @@ import {
 import { Button, ChakraProvider, Stack, Text } from '@chakra-ui/react';
 import { saveGeneralInfo } from '../imports/device/save-general-info';
 import { initializePackage } from '../imports/device/initialize-package';
-import { PACKAGE_NAME } from '../imports/device/package-name';
+import { PACKAGE_NAME } from '../imports/dialog/package-name';
 import { getBatteryInfo as saveBatteryInfo } from '../imports/device/save-battery-info';
 import { getLanguageId as saveLanguageId } from '../imports/device/save-language-id';
 import { getLanguageTag as saveLanguageTag } from '../imports/device/save-language-tag';
 import { Provider } from '../imports/provider';
+import {Dialog} from '@capacitor/dialog'
 
 function Content() {
   const deep = useDeep();
@@ -25,6 +26,55 @@ function Content() {
     'deviceLinkId',
     undefined
   );
+
+  // TODO Make query to subscribe to Alerts which are not executed yet
+  const {data: notExecutedAlertLinks} = useDeepSubscription({
+    type_id: {
+      _id: ["@deep-foundation/dialog", "Alert"]
+    },
+    _not: {
+      
+    }
+  })
+
+  useEffect(() => {
+    async function alertNotExecutedAlerts({alertLinkId}: {alertLinkId: number}) {
+      const alertTitleTypeLinkId = await deep.id(PACKAGE_NAME, "AlertTitle");
+      const alertMessageTypeLinkId = await deep.id(PACKAGE_NAME, "AlertMessage");
+      const alertButtonTitleTypeLinkId = await deep.id(PACKAGE_NAME, "AlertButtonTitle");
+
+      const {data: [{value: {value: title}}]} = await deep.select({
+        from: {
+          type_id: alertTitleTypeLinkId,
+          from_id: alertLinkId,
+        }
+      });
+
+      const {data: [{value: {value: message}}]} = await deep.select({
+        from: {
+          type_id: alertMessageTypeLinkId,
+          from_id: alertLinkId,
+        }
+      });
+
+      const {data: [{value: {value: buttonTitle}}]} = await deep.select({
+        from: {
+          type_id: alertButtonTitleTypeLinkId,
+          from_id: alertLinkId,
+        }
+      });
+
+      await Dialog.alert({
+        title,
+        message,
+        buttonTitle,
+      })
+    }
+
+    for (const notExecutedAlertLink of notExecutedAlertLinks) {
+      alertNotExecutedAlerts({alertLinkId: notExecutedAlertLink.id});
+    }   
+  }, [notExecutedAlertLinks])
 
   return (
     <Stack>
