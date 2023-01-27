4267025694d5c59a44c983ac875a976a6f4c5bec
FreePhoenix888 <freephoenix888@gmail.com>
2023-01-27 15:37:05 +0600

    Put code inside async function and call it

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,56 +2,60 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging/sw";
 import { onBackgroundMessage } from "firebase/messaging/sw";
 import {onFirebaseMessageCallback} from './../imports/push-notifications/onFirebaseMessageCallback';
-const { generateApolloClient } = require('@deep-foundation/hasura/client');
+import { generateApolloClient } from ('@deep-foundation/hasura/client');
 import dotenv from ('dotenv');
 dotenv.config();
 
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAdW-DEUZuYcN-1snWNcL7QvtkNdibT_vY',
-  authDomain: 'deep-97e93.firebaseapp.com',
-  projectId: 'deep-97e93',
-  storageBucket: 'deep-97e93.appspot.com',
-  messagingSenderId: '430972811028',
-  appId: '1:430972811028:web:7c43130f8166c437c03401',
-  measurementId: 'G-NJ1R8HDWLK',
-};
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = initializeApp(firebaseConfig);
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = getMessaging(firebaseApp);
-
-const apolloClient = generateApolloClient({
-  path: process.env.NEXT_PUBLIC_GQL_PATH || '', // <<= HERE PATH TO UPDATE
-  ssl: !!~process.env.NEXT_PUBLIC_GQL_PATH.indexOf('localhost')
-    ? false
-    : true,
-  // admin token in prealpha deep secret key
-  // token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsibGluayJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJsaW5rIiwieC1oYXN1cmEtdXNlci1pZCI6IjI2MiJ9LCJpYXQiOjE2NTYxMzYyMTl9.dmyWwtQu9GLdS7ClSLxcXgQiKxmaG-JPDjQVxRXOpxs',
-});
-const unloginedDeep = new DeepClient({ apolloClient });
-const guest = await unloginedDeep.guest();
-const guestDeep = new DeepClient({ deep: unloginedDeep, ...guest });
-const admin = await guestDeep.login({
-  linkId: await guestDeep.id('deep', 'admin'),
-});
-const deep = new DeepClient({ deep: guestDeep, ...admin });
-
-onBackgroundMessage(messaging, async (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
-  await insertPushNotificationToDeep({deep, deviceLinkId, payload})
-
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
+async function main () {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyAdW-DEUZuYcN-1snWNcL7QvtkNdibT_vY',
+    authDomain: 'deep-97e93.firebaseapp.com',
+    projectId: 'deep-97e93',
+    storageBucket: 'deep-97e93.appspot.com',
+    messagingSenderId: '430972811028',
+    appId: '1:430972811028:web:7c43130f8166c437c03401',
+    measurementId: 'G-NJ1R8HDWLK',
   };
+  // Initialize the Firebase app in the service worker by passing in
+  // your app's Firebase config object.
+  // https://firebase.google.com/docs/web/setup#config-object
+  const firebaseApp = initializeApp(firebaseConfig);
+  
+  // Retrieve an instance of Firebase Messaging so that it can handle background
+  // messages.
+  const messaging = getMessaging(firebaseApp);
+  
+  const apolloClient = generateApolloClient({
+    path: process.env.NEXT_PUBLIC_GQL_PATH || '', // <<= HERE PATH TO UPDATE
+    ssl: !!~process.env.NEXT_PUBLIC_GQL_PATH.indexOf('localhost')
+      ? false
+      : true,
+    // admin token in prealpha deep secret key
+    // token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsibGluayJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJsaW5rIiwieC1oYXN1cmEtdXNlci1pZCI6IjI2MiJ9LCJpYXQiOjE2NTYxMzYyMTl9.dmyWwtQu9GLdS7ClSLxcXgQiKxmaG-JPDjQVxRXOpxs',
+  });
+  const unloginedDeep = new DeepClient({ apolloClient });
+  const guest = await unloginedDeep.guest();
+  const guestDeep = new DeepClient({ deep: unloginedDeep, ...guest });
+  const admin = await guestDeep.login({
+    linkId: await guestDeep.id('deep', 'admin'),
+  });
+  const deep = new DeepClient({ deep: guestDeep, ...admin });
+  
+  onBackgroundMessage(messaging, async (payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    
+    await insertPushNotificationToDeep({deep, deviceLinkId, payload})
+  
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
+    };
+  
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
+}
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
+main()
