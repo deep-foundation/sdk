1a23a05c0a9b1a3836ad1a6a2ae6f5ae0dc69143
FreePhoenix888 <freephoenix888@gmail.com>
2023-02-05 07:10:39 +0000

    Use getIsPackageInstalled

--- imports/device/insert-package-to-deep.ts
+++ imports/device/insert-package-to-deep.ts
@@ -1,7 +1,7 @@
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
 import { PACKAGE_NAME } from "./package-name";
 
-export async function insertPackageLinksToDeep({deep}: {deep: DeepClient}) {
+export async function insertPackageToDeep({deep}: {deep: DeepClient}) {
 
   const typeTypeLinkId = await deep.id("@deep-foundation/core", "Type");
   const anyTypeLinkId = await deep.id("@deep-foundation/core", "Any");
