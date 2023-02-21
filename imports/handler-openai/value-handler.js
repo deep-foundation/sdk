d1a34f38c326e90c393433530ba6cee4fa2bc360
Vladimir <vova.krivosheya.2004@gmail.com>
2023-02-21 12:24:51 +0200

    add deep.select for apiKey

--- imports/handler-openai/value-handler.js
+++ imports/handler-openai/value-handler.js
@@ -1,7 +1,33 @@
-import { DeepClient} from "@deep-foundation/deeplinks/imports/client";
-
 async ({data: {newLink:openAiRequestLink},deep}) => {
+    const {Configuration, OpenAIApi} = require("openai")
+    const { DeepClient } = require('@deep-foundation/deeplinks/imports/client');
+    const containTypeLinkId = await deep.id("@deep-foundation/deep-openai", "Contain")
+
     const {data: [{value: {value}}]} = await deep.select({
         id: openAiRequestLink.to_id
     })
+    const {data: [{value: {apiKey}}]} = await deep.select({
+        type_id: openAiApiKeyTypeLinkId.to_id,
+        in: {
+            type_id: containTypeLinkId,
+            from_id: triggeredByLinkId
+        }
+    })
+
+    const configuration = new Configuration({
+        apiKey: apiKey,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: value,
+        temperature: 0.9,
+        max_tokens: 150,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0.6,
+        stop: [" Human:", " AI:"],
+    });
+    return response;
 }
