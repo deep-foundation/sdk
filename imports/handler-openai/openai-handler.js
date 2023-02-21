e00c4fab03f6eb101b63370ed575ac84ff11ccdf
Vladimir <vova.krivosheya.2004@gmail.com>
2023-02-21 11:48:52 +0200

    update links and add installPackage

--- imports/handler-openai/openai-handler.js
+++ imports/handler-openai/openai-handler.js
@@ -1,13 +1,14 @@
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
 import * as fs from "fs";
-import {PACKAGE_NAME} from "./openai-links";
 
-async function insertOpenAiHandler(deep){
+async function insertOpenAiHandler(){
+    const deep = new DeepClient({ deep: guestDeep, ...admin });
     export const PACKAGE_NAME = `@deep-foundation/deep-openai`
     const anyTypeLinkId = await deep.id(PACKAGE_NAME, "Any");
     const userTypeLinkId=await deep.id(PACKAGE_NAME, "User")
+    const userLinkId=await deep.id(PACKAGE_NAME, "User")
     const typeTypeLinkId = await deep.id(PACKAGE_NAME, "Type");
-    const fileTypeLinkId = await deep.id(PACKAGE_NAME, "SyncTextFile")
+    const syncTextFileTypeLinkId = await deep.id(PACKAGE_NAME, "SyncTextFile")
     const containTypeLinkId = await deep.id(PACKAGE_NAME, "Contain")
     const fileWithCodeOfHandlerName = "FileWithCodeOfHandlerName";
     const supportsJsLinkId = await deep.id(PACKAGE_NAME, "dockerSupportsJs" /* | "plv8SupportsJs" */)
@@ -15,23 +16,38 @@ async function insertOpenAiHandler(deep){
     const handlerName = "HandlerName";
     const handleOperationLinkId = await deep.id(PACKAGE_NAME, "HandleInsert" /* | HandleUpdate | HandleDelete */);
     const handleName = "HandleName";
-    const triggerTypeLinkId=(PACKAGE_NAME, "openAiRequestLink")
+    const triggerTypeLinkId=(PACKAGE_NAME, "openAiRequestTypeLinkId")
     const packageLinkId = await deep.id(PACKAGE_NAME, "Package");
+
+    const installPackage = async () => {
+        const apolloClient = generateApolloClient({
+            path: process.env.NEXT_PUBLIC_GQL_PATH || '', // <<= HERE PATH TO UPDATE
+            ssl: !!~process.env.NEXT_PUBLIC_GQL_PATH.indexOf('localhost')
+                ? false
+                : true,
+        });
+    }
+        const unloginedDeep = new DeepClient({ apolloClient });
+        const guest = await unloginedDeep.guest();
+        const guestDeep = new DeepClient({ deep: unloginedDeep, ...guest });
+        const admin = await guestDeep.login({
+            linkId: await guestDeep.id('deep', 'admin'),
+        });
 
     const code = fs.readFileSync('packages/sdk/imports/handler-openai/value-handler.js', {encoding: 'utf-8'});
 
     const { data: [{id:userInputLinkId}] } = await deep.insert({
-        type_id:fileTypeLinkId,
+        type_id:syncTextFileTypeLinkId,
+        string: { data: { value: "user input" } },
         in: {
             data: {
                 type_id: containTypeLinkId,
-                from_id: packageLinkId,
-                string: { data: { value: "user input" } }
+                from_id: userLinkId,
             },
         }
     })
 
-    const { data: [{id:openTypeLinkId}] } = await deep.insert({
+    const { data: [{id:openAiRequestTypeLinkId}] } = await deep.insert({
         type_id: typeTypeLinkId,
         from_id: userTypeLinkId,
         to_id: anyTypeLinkId,
@@ -39,7 +55,7 @@ async function insertOpenAiHandler(deep){
             data: {
                 type_id: containTypeLinkId,
                 from_id: packageLinkId,
-                string: {data: { value: "OpenTypeLinkId"}}
+                string: {data: { value: "OpenAiRequestTypeLinkId"}}
             },
         }
     });
@@ -49,7 +65,7 @@ async function insertOpenAiHandler(deep){
         in: {
             data: {
                 type_id: containTypeLinkId,
-                from_id: packageLinkId,
+                from_id: userLinkId,
                 string: {data: { value: "OpenAiApiKeyLinkId"}}
             },
         }
@@ -61,13 +77,13 @@ async function insertOpenAiHandler(deep){
         in: {
             data: {
                 type_id: containTypeLinkId,
-                from_id: packageLinkId,
+                from_id: userLinkId,
             },
         }
     });
 
     await deep.insert({
-        type_id: fileTypeLinkId,
+        type_id: syncTextFileTypeLinkId,
         in: {
             data: [
                 {
