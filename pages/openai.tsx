dc43337529a1f89dfe6f1e7288b6c72b96e379c2
flakeed <vova.krivosheya.2004@gmail.com>
2023-02-22 18:09:50 +0000

    add a file for creating user links in buttons

---
+++ pages/openai.tsx
@@ -0,0 +1,71 @@
+import {
+  LocalStoreProvider,
+  useLocalStore,
+} from '@deep-foundation/store/local';
+import {
+  DeepProvider,
+  useDeep,
+  useDeepSubscription,
+} from '@deep-foundation/deeplinks/imports/client';
+
+import { Button, ChakraProvider, Stack, Text } from '@chakra-ui/react';
+import { PACKAGE_NAME } from '../imports/openai/package-name';
+import { Provider } from '../imports/provider';
+
+function Content() {
+  const deep = useDeep();
+  const [openAiLinkId, setOpenAiLinkId] = useLocalStore(
+    'openAiLinkId',
+    undefined
+  );
+
+  return (
+    <Stack>
+      <Text suppressHydrationWarning>OpenAi link id: {openAiLinkId ?? " "}</Text>
+      <Button
+        onClick={async () => {
+          await deep.insert({
+            type_id: await deep.id(PACKAGE_NAME, "OpenAiRequest"),
+            string: { data: { value: process.env.OPENAI_API_KEY } },
+            in: {
+                data: {
+                    type_id: await deep.id('@deep-foundation/core', "Contain"),
+                    from_id: deep.linkId,
+                },
+            }
+        });
+        }}
+      >
+        add openAiApiKeyLinkId link
+      </Button>
+      <Button
+        onClick={async () => {
+          await deep.insert({
+            type_id: await deep.id('@deep-foundation/core', "SyncTextFile"),
+            string: { data: { value: "user input" } },
+            in: {
+              data: {
+                type_id: await deep.id('@deep-foundation/core', "Contain"),
+                from_id: deep.linkId,
+              },
+            },
+          });
+        }}
+      >
+        add userInputLinkId link
+      </Button>
+    </Stack>
+  );
+}
+
+export default function OpenaiPage() {
+  return (
+    <ChakraProvider>
+      <Provider>
+        <DeepProvider>
+          <Content />
+        </DeepProvider>
+      </Provider>
+    </ChakraProvider>
+  );
+}
