e072ed98230e4d6f912cde081c7def35a1d3073f
Vladimir <58123600+flakeed@users.noreply.github.com>
2023-02-22 13:50:49 +0200

    rename to OPENAI_PACKAGE_NAME
    Co-authored-by: FreePhoenix888 <freephoenix888@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,7 @@ function Page() {
         return;
       }
       
-      if (!await getIsPackageInstalled({deep, packageName: DEVICE_PACKAGE_NAME})) {
+      if (!await getIsPackageInstalled({deep, packageName: OPENAI_PACKAGE_NAME})) {
         await insertDevicePackageToDeep({ deep });
       }
       if (!await getIsPackageInstalled({deep, packageName: DEVICE_PACKAGE_NAME})) {
