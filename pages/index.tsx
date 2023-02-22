2cfab31dd1e8dd147e88418753cefece13fa71b9
Vladimir <58123600+flakeed@users.noreply.github.com>
2023-02-22 13:50:03 +0200

    set name insertDevicePackageToDeep for insertPackageToDeep
    Co-authored-by: FreePhoenix888 <freephoenix888@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ import {
 } from '@deep-foundation/deeplinks/imports/client';
 import Link from 'next/link';
 import { insertPackageToDeep as insertDevicePackageToDeep } from '../imports/device/insert-package-to-deep';
-import { insertPackageToDeep as insertDevicePackageToDeep } from '../imports/device/insert-package-to-deep';
+import { insertPackageToDeep as insertOpenAiPackageToDeep } from '../imports/openai/insert-package-to-deep';
 import { PACKAGE_NAME as DEVICE_PACKAGE_NAME } from '../imports/device/package-name';
 import { PACKAGE_NAME as DEVICE_PACKAGE_NAME } from '../imports/device/package-name';
 import { getIsPackageInstalled } from '../imports/get-is-package-installed';
