7db0599f790eb4c0ae3a27e3c43a379fc00f2278
FreePhoenix888 <freephoenix888@gmail.com>
2023-02-24 10:25:59 +0000

    Rename

--- imports/push-notification/install-package.ts
+++ imports/push-notification/install-package.ts
@@ -2,9 +2,27 @@ import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
 import { PACKAGE_NAME } from "./package-name";
 import { PACKAGE_NAME as DEVICE_PACKAGE_NAME } from "../device/package-name";
 import * as fs from 'fs';
+import { generateApolloClient } from '@deep-foundation/hasura/client';
+import * as dotenv from 'dotenv';
 
+async function installPackage() {
+  const apolloClient = generateApolloClient({
+    path: process.env.NEXT_PUBLIC_GQL_PATH || '', // <<= HERE PATH TO UPDATE
+    ssl: !!~process.env.NEXT_PUBLIC_GQL_PATH.indexOf('localhost')
+      ? false
+      : true,
+    // admin token in prealpha deep secret key
+    // token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsibGluayJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJsaW5rIiwieC1oYXN1cmEtdXNlci1pZCI6IjI2MiJ9LCJpYXQiOjE2NTYxMzYyMTl9.dmyWwtQu9GLdS7ClSLxcXgQiKxmaG-JPDjQVxRXOpxs',
+  });
 
-export async function insertPackageToDeep({deep}:{deep: DeepClient}) {
+  const unloginedDeep = new DeepClient({ apolloClient });
+  const guest = await unloginedDeep.guest();
+  const guestDeep = new DeepClient({ deep: unloginedDeep, ...guest });
+  const admin = await guestDeep.login({
+    linkId: await guestDeep.id('deep', 'admin'),
+  });
+  const deep = new DeepClient({ deep: guestDeep, ...admin });
+
   const typeTypeLinkId = await deep.id(
     '@deep-foundation/core',
     'Type'
@@ -351,3 +369,5 @@ export async function insertPackageToDeep({deep}:{deep: DeepClient}) {
     },
   });
 };
+
+installPackage();
