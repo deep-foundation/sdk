a02dd07012a7c5c24613bb91e300602d4df244f7
flakeed <vova.krivosheya.2004@gmail.com>
2023-02-24 12:26:05 +0000

    add apiKey.value.value

--- imports/openai/request-handler.js
+++ imports/openai/request-handler.js
@@ -1,7 +1,20 @@
-async ({deep,require,data: {newLink:openAiRequestLink,triggeredByLinkId}}) => {
+async ({data: {newLink:openAiRequestLink,triggeredByLinkId},deep,require}) => {
     const {Configuration, OpenAIApi} = require("openai")
+    const CircularJSON = require('circular-json');
     const containTypeLinkId = await deep.id("@deep-foundation/core", "Contain")
-
+    const openAiApiKeyTypeLinkId = await deep.id(`@deep-foundation/openai`, "OpenAiApiKey")
+   //add this for fix 
+    // await deep.insert({
+    //     type_id: await deep.id(`@deep-foundation/core`, "Value"),
+    //     from_id: await deep.id(`@deep-foundation/openai`, "OpenAiApiKey"),
+    //     to_id: await deep.id(`@deep-foundation/core`, "String"),
+    //     in: {
+    //         data: {
+    //             type_id: await deep.id(`@deep-foundation/core`, "Contain"),
+    //             from_id: await deep.id(`@deep-foundation/core`, "Package"),
+    //         },
+    //     }
+    // });
     const {data: [{value: {value}}]} = await deep.select({
         id: openAiRequestLink.to_id
     })
@@ -13,18 +26,18 @@ async ({deep,require,data: {newLink:openAiRequestLink,triggeredByLinkId}}) => {
         }
     })
     if(!apiKeyLink){
-      throw new Exception(`A link with type ##${openAiApiKeyTypeLinkId} is not found`)
+        throw new Error(`A link with type ##${openAiApiKeyTypeLinkId} is not found`)
     }
     if(!apiKeyLink.value?.value){
-      throw new Exception(`##${apiKeyLink.id} must have a value`)
+        throw new Error(`##${apiKeyLink.id} must have a value`)
     }
-
+    const apiKey = apiKeyLink.value.value
     const configuration = new Configuration({
         apiKey: apiKey,
     });
     const openai = new OpenAIApi(configuration);
 
-    const response = await openai.createCompletion({
+    const response = CircularJSON.stringify(await openai.createCompletion({
         model: "text-davinci-003",
         prompt: value,
         temperature: 0.9,
@@ -33,6 +46,7 @@ async ({deep,require,data: {newLink:openAiRequestLink,triggeredByLinkId}}) => {
         frequency_penalty: 0,
         presence_penalty: 0.6,
         stop: [" Human:", " AI:"],
-    });
+    }));
+   
     return response;
 }
