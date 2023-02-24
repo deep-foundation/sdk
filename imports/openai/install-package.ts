f212c73c3b0b8b8d0c9ac1b61270cfeca369726c
flakeed <vova.krivosheya.2004@gmail.com>
2023-02-24 09:37:55 +0000

    add deep

--- imports/openai/install-package.ts
+++ imports/openai/install-package.ts
@@ -1,14 +1,25 @@
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
 import fs from 'fs';
-import {
-    DeepProvider,
-    useDeep,
-    useDeepSubscription,
-  } from '@deep-foundation/deeplinks/imports/client';
+const { generateApolloClient } = require('@deep-foundation/hasura/client');
 import {PACKAGE_NAME} from "./package-name";
 require('dotenv').config();
 export async function installPackage () {
-    const deep = useDeep();
+    const apolloClient = generateApolloClient({
+        path: process.env.NEXT_PUBLIC_GQL_PATH || '', // <<= HERE PATH TO UPDATE
+        ssl: !!~process.env.NEXT_PUBLIC_GQL_PATH.indexOf('localhost')
+          ? false
+          : true,
+        // admin token in prealpha deep secret key
+        // token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsibGluayJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJsaW5rIiwieC1oYXN1cmEtdXNlci1pZCI6IjI2MiJ9LCJpYXQiOjE2NTYxMzYyMTl9.dmyWwtQu9GLdS7ClSLxcXgQiKxmaG-JPDjQVxRXOpxs',
+      });
+    
+    const unloginedDeep = new DeepClient({ apolloClient });
+    const guest = await unloginedDeep.guest();
+    const guestDeep = new DeepClient({ deep: unloginedDeep, ...guest });
+    const admin = await guestDeep.login({
+      linkId: await guestDeep.id('deep', 'admin'),
+    });
+    const deep = new DeepClient({ deep: guestDeep, ...admin });
 
     const anyTypeLinkId = await deep.id('@deep-foundation/core', "Any");
     const userTypeLinkId = await deep.id('@deep-foundation/core', "User");
