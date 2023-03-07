4cef6db641b65dd9bf96be212c83e6db3a43cc00
FreePhoenix888 <freephoenix888@gmail.com>
2023-03-07 13:48:43 +0000

    Format

--- imports/device/insert-battery-info-to-deep.ts
+++ imports/device/insert-battery-info-to-deep.ts
@@ -2,22 +2,22 @@ import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
 import { PACKAGE_NAME } from "./package-name";
 import { BatteryInfo, Device } from "@capacitor/device";
 
-export async function insertBatteryInfoToDeep({deep, deviceLinkId, deviceBatteryInfo}: {deep: DeepClient, deviceLinkId: number, deviceBatteryInfo: BatteryInfo}) {
-	
-  if(!deviceLinkId) {
-		throw new Error("deviceLinkId must not be 0")
-	}
+export async function insertBatteryInfoToDeep({ deep, deviceLinkId, deviceBatteryInfo }: { deep: DeepClient, deviceLinkId: number, deviceBatteryInfo: BatteryInfo }) {
 
-	const containTypeLinkId = await deep.id('@deep-foundation/core', 'Contain');
+  if (!deviceLinkId) {
+    throw new Error("deviceLinkId must not be 0")
+  }
+
+  const containTypeLinkId = await deep.id('@deep-foundation/core', 'Contain');
   const batteryLevelTypeLinkId = await deep.id(PACKAGE_NAME, 'BatteryLevel');
   const chargingStateTypeLinkId = await deep.id(PACKAGE_NAME, "ChargingState");
   const isChargingTypeLinkId = await deep.id(PACKAGE_NAME, 'IsCharging');
   const isNotChargingTypeLinkId = await deep.id(PACKAGE_NAME, 'IsNotCharging');
 
-	await deep.insert([
+  await deep.insert([
     deviceBatteryInfo.batteryLevel && {
       type_id: batteryLevelTypeLinkId,
-      number: { data: { value: Math.floor(deviceBatteryInfo.batteryLevel*100) } },
+      number: { data: { value: Math.floor(deviceBatteryInfo.batteryLevel * 100) } },
       in: {
         data: {
           type_id: containTypeLinkId,
