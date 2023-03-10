062d7902d1495d9a789d3a7b8780a5a8c9764fe7
FreePhoenix888 <freephoenix888@gmail.com>
2023-03-10 16:46:33 +0000

    Add and use insertServiceAccount

---
+++ imports/push-notification/insert-service-account.ts
@@ -0,0 +1,41 @@
+import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
+import { PACKAGE_NAME } from "./package-name";
+
+export async function insertServiceAccount({deep, serviceAccount}: {deep: DeepClient, serviceAccount: string}) {
+  const containTypeLinkId = await deep.id(
+    '@deep-foundation/core',
+    'Contain'
+  );
+  const serviceAccountTypeLinkId = await deep.id(
+    PACKAGE_NAME,
+    'ServiceAccount'
+  );
+  const usesServiceAccountTypeLinkId = await deep.id(
+    PACKAGE_NAME,
+    'UsesServiceAccount'
+  );
+
+  await deep.insert({
+    type_id: usesServiceAccountTypeLinkId,
+    from_id: deep.linkId,
+    to: {
+      data: {
+        type_id: serviceAccountTypeLinkId,
+        object: {
+          data: {
+            value: serviceAccount,
+          },
+        },
+        in: {
+          data: [
+            {
+              type_id: containTypeLinkId,
+              from_id: deep.linkId,
+            },
+          ],
+        },
+      }
+    }
+  });
+
+}
--- pages/push-notification.tsx
+++ pages/push-notification.tsx
@@ -35,6 +35,7 @@ import { insertOrUpdateDeviceRegistrationToken } from '../imports/push-notificat
 import { PACKAGE_NAME } from '../imports/push-notification/package-name';
 import { requestPermissions } from '../imports/push-notification/request-permissions';
 import { insertWebPushCertificate } from '../imports/push-notification/insert-web-push-certificate';
+import { insertServiceAccount } from '../imports/push-notification/insert-service-account';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAdW-DEUZuYcN-1snWNcL7QvtkNdibT_vY',
@@ -165,58 +166,10 @@ npx ts-node "./imports/\${package_name}/install-package.ts"
       </Text>
       <Button
         onClick={async () => {
-          const containTypeLinkId = await deep.id(
-            '@deep-foundation/core',
-            'Contain'
-          );
-          const serviceAccountTypeLinkId = await deep.id(
-            PACKAGE_NAME,
-            'ServiceAccount'
-          );
-          console.log({ deviceLinkId });
-
-          await deep.delete({
-            down: {
-              parent: {
-                type_id: containTypeLinkId,
-                from_id: deviceLinkId,
-                to: {
-                  type_id: serviceAccountTypeLinkId,
-                },
-              },
-            },
-          });
-          await deep.insert({
-            type_id: serviceAccountTypeLinkId,
-            object: {
-              data: {
-                value: {
-                  type: 'service_account',
-                  project_id: 'deep-97e93',
-                  private_key_id: 'cb08d26ecf3eaa62ec65da7520e895745ac84aca',
-                  private_key:
-                    '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCkZt+FoADKf3dG\n3NLiN3KqRc5rRda2WkSZ2YSmWMf+6hD49P4zigIX4m3G5WeyylcxhITrEA+ApeEC\n6IyjAqCciEco48YrKI3OL7vYgjnEp4he+6QMa4UGQn6D2wRGeNUBM8Ht/Z435Rhq\natlnLRs8tSMqD3MG0W+W9AwSQdoAUm6ETLBXlnOiBaUwIiEYDxXEEEHPMW0+IIPN\nkXdGwDqTOYBGIUqrD2GUu0NvVi5GFQsRA5b3dJ9ICQZh4AdClm+Hyg02JPp+ubgZ\nF7hJnidtPIJjnBSNIaRKwoqkQ0OMJr0CBZwVZfWX4W1wZDRIl2ECgu98LylykE7U\n5h4kU3Z1AgMBAAECggEAAuhUUbhjiB2ZaxG+dNDLHP7Ythhllckk8n1jFraYrEhp\nWmTk1BjdTVgHYs0wCcGe7n/lx1xA1e9+BBbTN5whdXjKYdn/uBalKChsnSgMS53l\n5ipzWQCACAb7rwPNJ/WGsXuTmmVIFtAYJNCXYCOWt8l3epGp9K9RSAwJFe0JafWr\nYc13ujhnrv6eMSF0Amkf4IfN2fpybVK61l2LmxA4PXdp1LnPbG0F+Wp5ezOcW8zr\nk6fsbRYhgNHppB1RYSqD91utiw4VqWdIrkj+x10uf0BKV1ioHHnSv91jJ7S2ojPY\nd5OkAHBGLHjBQJwQpYdYaLyk4c/wIRhLyrRmwgfy6QKBgQDRk9dnImwXuk0ICeB+\nOmaOt1ByiRNEv7l9v93YODohXFH+fQCYPzpzd96EiFlr3Ginvnm0xxMJ6es0Iv+J\noNZdCTvWWNsn4CktMlIGuG1eoB1NOMXR3O47DTjG1Pfl1mX81vd+Gmvk35jlpVgu\n2jiJHouXdQBHNTNg8RPmSM2y7QKBgQDI0VTxeP52FVT8d62mAU2nH04B6NLq/ogE\nQLbeV2k0LLKccRwlB6QhseEn9GBWO2YyGk5d35tid9qECgnvSzOHHrI83q/ysvKS\npo3MtKct/br1nssvF0Jb83HQLMBFXoXSZPEHr1G3j9eBpJrnt16W5yCXIlFMCYa3\nLHc1B+a4qQKBgQC2blMa28PsA6f6T16zgnKz2K6WhOvY9GurItEh3g/76jkVpgpW\nfPOMf9Oa2nW8hmgzXILk8kWIY67x+2UlkHQJGHiV5VMgKuitBxiP8QqDTC01gy3v\nuLlHfgLmUYxY7YBpz1Yw1x8EY/7cKEnSvvJnqccpWBed5JmM0U3ZL5afIQKBgATy\n26QDtkmUpv59uILBv3ch11tGsIPn99QbACakgswtWc1vICFtecb2yjSg2grl9dPA\nQDQiAYNArtrYIHyMHt3yjLPhTPavIDkq742e2gvRF91bp2gmq5T2f9SAddB/zs5r\nfGETGfFrEwV31/Vj2GnhkjUHPHeiOHalGt7srevZAoGBAMmm2iLA7x+bHoSsLHl6\nqOs2D5AdoJji7YkBPLpIpN4gTmReUhsTtqThe2vzepODATjyo49QuC+jFemiGnxa\nNljhUeNkt5yHi2hl9xfjSKB/81bwHhz617VsPMt/lf1dyFi4jLCnsFihoRF5HwJ3\nDjzsmfCl8njOqGs1JeeaeLOn\n-----END PRIVATE KEY-----\n',
-                  client_email:
-                    'firebase-adminsdk-j162x@deep-97e93.iam.gserviceaccount.com',
-                  client_id: '102324677571727204105',
-                  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-                  token_uri: 'https://oauth2.googleapis.com/token',
-                  auth_provider_x509_cert_url:
-                    'https://www.googleapis.com/oauth2/v1/certs',
-                  client_x509_cert_url:
-                    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-j162x%40deep-97e93.iam.gserviceaccount.com',
-                },
-              },
-            },
-            in: {
-              data: [
-                {
-                  type_id: containTypeLinkId,
-                  from_id: deep.linkId,
-                },
-              ],
-            },
-          });
+          await insertServiceAccount({
+            deep,
+            serviceAccount
+          })
         }}
       >
         Insert Default Service Account
