a6e999b520616b976da42ca0da2e1e47b7865b40
FreePhoenix888 <freephoenix888@gmail.com>
2023-03-11 09:48:33 +0000

    Add and use registerDevice

---
+++ imports/push-notification/register-device.ts
@@ -0,0 +1,67 @@
+import { DeviceInfo } from "@capacitor/device";
+import { PushNotifications } from "@capacitor/push-notifications";
+import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
+import { getToken, Messaging } from "firebase/messaging";
+import { insertDeviceRegistrationToken } from "./insert-device-registration-token";
+import { PACKAGE_NAME } from "./package-name";
+
+export async function registerDevice({ deep, deviceLinkId, platform, firebaseMessaging, callback }: { deep: DeepClient, deviceLinkId: number, platform: DeviceInfo["platform"], firebaseMessaging: Messaging, callback: ({deviceRegistrationTokenLinkId, deviceRegistrationToken}:{deviceRegistrationTokenLinkId: number, deviceRegistrationToken: string}) => void }) {
+  const onDeviceRegistration = async ({ value: deviceRegistrationToken }: { value: string }) => {
+    const { deviceRegistrationTokenLinkId } = await insertDeviceRegistrationToken({
+      deep,
+      deviceRegistrationToken,
+      deviceLinkId,
+    });
+    await callback({deviceRegistrationTokenLinkId, deviceRegistrationToken});
+  };
+
+  if (platform === 'web') {
+    const serviceWorkerRegistration =
+      await navigator.serviceWorker.register(
+        './firebase-messaging-sw.js',
+        { scope: 'firebase-cloud-messaging-push-scope' }
+      );
+    const containTypeLinkId = await deep.id(
+      '@deep-foundation/core',
+      'Contain'
+    );
+    const webPushCertificateTypeLinkId = await deep.id(
+      PACKAGE_NAME,
+      'WebPushCertificate'
+    );
+    const {
+      data: [webPushCertificateLink],
+    } = await deep.select({
+      type_id: webPushCertificateTypeLinkId,
+      in: {
+        type_id: containTypeLinkId,
+        from_id: deep.linkId,
+      },
+    });
+    if (!webPushCertificateLink) {
+      throw new Error(
+        `A link with type ${webPushCertificateTypeLinkId} is not found`
+      );
+    }
+    if (!webPushCertificateLink.value?.value) {
+      throw new Error(`${webPushCertificateLink} must have a value`);
+    }
+    const webPushCertificate = webPushCertificateLink.value.value;
+    console.log({ webPushCertificateLink })
+    console.log({ webPushCertificate })
+    const deviceRegistrationToken = await getToken(firebaseMessaging, {
+      serviceWorkerRegistration,
+      vapidKey: webPushCertificate,
+    });
+
+    onDeviceRegistration({ value: deviceRegistrationToken })
+  } else {
+    await PushNotifications.removeAllListeners();
+    await PushNotifications.addListener(
+      'registration',
+      onDeviceRegistration
+    );
+    await PushNotifications.register();
+  }
+
+}
