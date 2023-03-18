63c85ebff3253c874caf09e77972ddc2672a1632
Roman <technopriest666@gmail.com>
2023-03-18 22:00:22 +0300

    Tree include added

--- imports/audiorecord/initialize-package.ts
+++ imports/audiorecord/initialize-package.ts
@@ -1,8 +1,6 @@
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
 
 export const PACKAGE_NAME = "@deep-foundation/audiorecord"
-export const PACKAGE_TYPES =
-  ["DeviceSupport", "Permissions", "AudioRecords", "Record", "Duration", "StartTime", "EndTime", "Sound", "MIME/type"]
 
 export default async function initializePackage(deep: DeepClient, deviceLinkId) {
   const packageTypeLinkId = await deep.id('@deep-foundation/core', "Package")
@@ -10,6 +8,17 @@ export default async function initializePackage(deep: DeepClient, deviceLinkId) 
   const joinTypeLinkId = await deep.id("@deep-foundation/core", "Join")
   const typeTypeLinkId = await deep.id("@deep-foundation/core", "Type")
   const treeTypeLinkId = await deep.id('@deep-foundation/core', 'Tree');
+  const anyTypeLinkId = await deep.id("@deep-foundation/core", "Any");
+  const treeIncludeNodeTypeLinkId = await deep.id(
+    '@deep-foundation/core',
+    'TreeIncludeNode'
+  );
+  const treeIncludeUpTypeLinkId = await deep.id('@deep-foundation/core', 'TreeIncludeUp');
+  const treeIncludeDownTypeLinkId = await deep.id(
+    '@deep-foundation/core',
+    'TreeIncludeDown'
+  );
+
 
   const { data: [{ id: packageLinkId }] } = await deep.insert({
     type_id: packageTypeLinkId,
@@ -42,16 +51,219 @@ export default async function initializePackage(deep: DeepClient, deviceLinkId) 
     }
   })
 
-  const { data: [{ id: pacakgeTypeLinkId }] } = await deep.insert(PACKAGE_TYPES.map((TYPE) => ({
-    type_id: typeTypeLinkId,
-    in: {
-      data: [{
-        type_id: containTypeLinkId,
-        from_id: packageLinkId,
-        string: { data: { value: TYPE } }
-      }]
-    }
-  })))
+  await deep.insert([
+    {
+      type_id: typeTypeLinkId,
+      in: {
+        data: [{
+          type_id: containTypeLinkId,
+          from_id: packageLinkId,
+          string: { data: { value: 'AudioRecords' } },
+        },
+        {
+          type_id: treeIncludeNodeTypeLinkId,
+          from_id: dialogTreeLinkId,
+          in: {
+            data: [
+              {
+                type_id: containTypeLinkId,
+                from_id: packageLinkId,
+              },
+            ],
+          },
+        }]
+      },
+      out: {
+        data: [
+          {
+            type_id: typeTypeLinkId,
+            to_id: anyTypeLinkId,
+            in: {
+              data: [{
+                type_id: containTypeLinkId,
+                from_id: packageLinkId,
+                string: { data: { value: 'Permissions' } },
+              }, {
+                type_id: treeIncludeDownTypeLinkId,
+                from_id: dialogTreeLinkId,
+                in: {
+                  data: [
+                    {
+                      type_id: containTypeLinkId,
+                      from_id: packageLinkId,
+                    },
+                  ],
+                },
+              }]
+            }
+          },
+          {
+            type_id: typeTypeLinkId,
+            to_id: anyTypeLinkId,
+            in: {
+              data: [{
+                type_id: containTypeLinkId,
+                from_id: packageLinkId,
+                string: { data: { value: 'DeviceSupport' } },
+              }, {
+                type_id: treeIncludeDownTypeLinkId,
+                from_id: dialogTreeLinkId,
+                in: {
+                  data: [
+                    {
+                      type_id: containTypeLinkId,
+                      from_id: packageLinkId,
+                    },
+                  ],
+                },
+              }]
+            }
+          },
+        ]
+      }
+    },
+    {
+      type_id: typeTypeLinkId,
+      in: {
+        data: [{
+          type_id: containTypeLinkId,
+          from_id: packageLinkId,
+          string: { data: { value: 'Record' } },
+        },
+        {
+          type_id: treeIncludeNodeTypeLinkId,
+          from_id: dialogTreeLinkId,
+          in: {
+            data: [
+              {
+                type_id: containTypeLinkId,
+                from_id: packageLinkId,
+              },
+            ],
+          },
+        }]
+      },
+      out: {
+        data: [
+          {
+            type_id: typeTypeLinkId,
+            to_id: anyTypeLinkId,
+            in: {
+              data: [{
+                type_id: containTypeLinkId,
+                from_id: packageLinkId,
+                string: { data: { value: 'Duration' } },
+              }, {
+                type_id: treeIncludeDownTypeLinkId,
+                from_id: dialogTreeLinkId,
+                in: {
+                  data: [
+                    {
+                      type_id: containTypeLinkId,
+                      from_id: packageLinkId,
+                    },
+                  ],
+                },
+              }]
+            }
+          },
+          {
+            type_id: typeTypeLinkId,
+            to_id: anyTypeLinkId,
+            in: {
+              data: [{
+                type_id: containTypeLinkId,
+                from_id: packageLinkId,
+                string: { data: { value: 'StartTime' } },
+              }, {
+                type_id: treeIncludeDownTypeLinkId,
+                from_id: dialogTreeLinkId,
+                in: {
+                  data: [
+                    {
+                      type_id: containTypeLinkId,
+                      from_id: packageLinkId,
+                    },
+                  ],
+                },
+              }]
+            }
+          },
+          {
+            type_id: typeTypeLinkId,
+            to_id: anyTypeLinkId,
+            in: {
+              data: [{
+                type_id: containTypeLinkId,
+                from_id: packageLinkId,
+                string: { data: { value: 'EndTime' } },
+              }, {
+                type_id: treeIncludeDownTypeLinkId,
+                from_id: dialogTreeLinkId,
+                in: {
+                  data: [
+                    {
+                      type_id: containTypeLinkId,
+                      from_id: packageLinkId,
+                    },
+                  ],
+                },
+              }]
+            }
+          },
+          {
+            type_id: typeTypeLinkId,
+            to_id: anyTypeLinkId,
+            in: {
+              data: [{
+                type_id: containTypeLinkId,
+                from_id: packageLinkId,
+                string: { data: { value: 'Sound' } },
+              }, {
+                type_id: treeIncludeDownTypeLinkId,
+                from_id: dialogTreeLinkId,
+                in: {
+                  data: [
+                    {
+                      type_id: containTypeLinkId,
+                      from_id: packageLinkId,
+                    },
+                  ],
+                },
+              }]
+            }
+          },
+          {
+            type_id: typeTypeLinkId,
+            to_id: anyTypeLinkId,
+            in: {
+              data: [{
+                type_id: containTypeLinkId,
+                from_id: packageLinkId,
+                string: { data: { value: 'MIME/type' } },
+              }, {
+                type_id: treeIncludeDownTypeLinkId,
+                from_id: dialogTreeLinkId,
+                in: {
+                  data: [
+                    {
+                      type_id: containTypeLinkId,
+                      from_id: packageLinkId,
+                    },
+                  ],
+                },
+              }]
+            }
+          }
+        ]
+      }
+    },
+  ]);
+
+  // await deep.insert([
+    
+  // ]);
+
   const { data: [{ id: AudioRecordsLinkId }] } = await deep.insert({
     type_id: await deep.id(PACKAGE_NAME, "AudioRecords"),
     in: {
@@ -62,5 +274,7 @@ export default async function initializePackage(deep: DeepClient, deviceLinkId) 
       }
     }
   })
+
   console.log("audiorecord package installed");
 }
+
