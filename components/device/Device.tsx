a54ec7ac336bc4302fb8593328b1bebc3131e915
FreePhoenix888 <freephoenix888@gmail.com>
2023-03-25 07:51:33 +0000

    Add device component

---
+++ components/device/Device.tsx
@@ -0,0 +1,20 @@
+import { DeviceInfo } from "@capacitor/device";
+import { Card, CardHeader, Heading, CardBody, Stack, StackDivider, Box, Text, Image } from "@chakra-ui/react";
+import { Device } from "../../imports/device/device";
+
+export function Device({ device }: { device: Device }) {
+  return <Card >
+    <CardHeader>
+      <Heading >{
+        device.name
+      }</Heading>
+    </CardHeader>
+    <CardBody>
+      <Text>
+        {
+          device.model
+        }
+      </Text>
+    </CardBody>
+  </Card>
+}
