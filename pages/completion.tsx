5c2fa0b095faf89ea7dc93206165b7e06859c6bc
flakeed <vova.krivosheya.2004@gmail.com>
2023-04-11 19:32:59 +0000

    update page for completion

---
+++ pages/completion.tsx
@@ -0,0 +1,114 @@
+import {
+  LocalStoreProvider,
+  useLocalStore,
+} from '@deep-foundation/store/local';
+import {
+  DeepProvider,
+  useDeep,
+  useDeepSubscription,
+} from '@deep-foundation/deeplinks/imports/client';
+import { Button, ChakraProvider, Stack, Text } from '@chakra-ui/react';
+const PACKAGE_NAME='@flakeed/openai';
+import { Provider } from '../imports/provider';
+
+function Content() {
+  const deep = useDeep();
+  const [openAiLinkId, setOpenAiLinkId] = useLocalStore(
+    'openAiLinkId',
+    undefined
+  );
+
+  return (
+    <Stack>
+      <Text suppressHydrationWarning>OpenAi link id: {openAiLinkId ?? " "}</Text>
+      <Button
+        onClick={async () => {
+          await deep.insert({
+            type_id: await deep.id(PACKAGE_NAME, "ApiKey"),
+            string: { data: { value: process.env.OPENAI_API_KEY } },
+            in: {
+                data: {
+                    type_id: await deep.id('@deep-foundation/core', "Contain"),
+                    from_id: deep.linkId,
+                },
+            }
+        });
+        }}
+      >
+        add openAiApiKeyLinkId link
+      </Button>
+      <Button
+        onClick={async () => {
+          await deep.insert({
+            type_id: await deep.id('@deep-foundation/core', "SyncTextFile"),
+            string: { data: { value: "user input" } },
+            in: {
+              data: {
+                type_id: await deep.id('@deep-foundation/core', "Contain"),
+                from_id: deep.linkId,
+              },
+            },
+          });
+        }}
+      >
+        add userInputLinkId link
+      </Button>
+      <Button
+        onClick={async () => {
+          let makeActive = true;
+          if (makeActive) {
+            await deep.delete({
+              up: {
+                tree_id: { _eq: await deep.id("@deep-foundation/core", "containTree") },
+                parent: {
+                  type_id: { _id: ["@deep-foundation/core", "Contain"] },
+                  to: { type_id: await deep.id(PACKAGE_NAME, "UsesOpenAiApiKey"), },
+                  from_id: deep.linkId
+                }
+              }
+            })
+          }
+          
+          await deep.insert({
+            type_id:  await deep.id(PACKAGE_NAME, "ApiKey"),
+            string: { data: { value: process.env.OPENAI_API_KEY }},
+            in: {
+              data: [
+                {
+                  type_id: await deep.id('@deep-foundation/core', "Contain"),
+                  from_id: deep.linkId,
+                },
+                makeActive && {
+                  type_id:  await deep.id(PACKAGE_NAME, "UsesOpenAiApiKey"),
+                  from_id: deep.linkId,
+                  in: {
+                    data: [
+                      {
+                        type_id: await deep.id('@deep-foundation/core', "Contain"),
+                        from_id: deep.linkId,
+                      },
+                    ],
+                  },
+                }
+              ],
+            },
+          })
+        }}
+      >
+        add ApiKey and UsesOpenAiApiKey links
+      </Button>
+    </Stack>
+  );
+}
+
+export default function OpenaiPage() {
+  return (
+    <ChakraProvider>
+      <Provider>
+        <DeepProvider>
+          <Content />
+        </DeepProvider>
+      </Provider>
+    </ChakraProvider>
+  );
+}
