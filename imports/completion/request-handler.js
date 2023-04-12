533d1a462924a65006b5e14270867904e9ba1a31
Vladimir <58123600+flakeed@users.noreply.github.com>
2023-04-12 16:36:29 +0300

    Update check apikey

--- imports/completion/request-handler.js
+++ imports/completion/request-handler.js
@@ -1,7 +1,8 @@
 async ({data: {newLink:openAiRequestLink,triggeredByLinkId},deep,require}) => {
   const {Configuration, OpenAIApi} = require("openai")
-  const apiKeyTypeLinkId = await deep.id('@flakeed/openai', "ApiKey")
-  const usesApiKeyTypeLinkId = await deep.id('@flakeed/openai', "UsesApiKey")
+  const apiKeyTypeLinkId = await deep.id('@deep-foundation/openai', "ApiKey")
+  const usesApiKeyTypeLinkId = await deep.id('@deep-foundation/openai', "UsesApiKey")
+    const containTypeLinkId = await deep.id('@deep-foundation/core', "Contain")
 
   const {data: [linkWithStringValue]} = await deep.select({
       id: openAiRequestLink.to_id
@@ -11,21 +12,8 @@ async ({data: {newLink:openAiRequestLink,triggeredByLinkId},deep,require}) => {
   }
   const openAiPrompt = linkWithStringValue.value.value
 
-  const { data: [apiKeyLink] } = await deep.select({
-      type_id: apiKeyTypeLinkId,
-      in: {
-        type_id: usesApiKeyTypeLinkId,
-        from_id: triggeredByLinkId
-      }
-    });
-
-  if(!apiKeyLink){
-      throw new Error(`A link with type ##${apiKeyTypeLinkId} is not found`)
-  }
-  if(!apiKeyLink.value?.value){
-      throw new Error(`##${apiKeyLink.id} must have a value`)
-  }
-  const apiKey = apiKeyLink.value.value
+  const apiKeyLink = await getTokenLink();
+  const apiKey = apiKeyLink.value.value;
   const configuration = new Configuration({
       apiKey: apiKey,
   });
@@ -41,5 +29,69 @@ async ({data: {newLink:openAiRequestLink,triggeredByLinkId},deep,require}) => {
       presence_penalty: 0.6,
       stop: [" Human:", " AI:"],
   });
+
+  async function getTokenLink() {
+    let resultTokenLink;
+    const { data } = await deep.select({
+        _or: [
+            {
+                type_id: apiKeyTypeLinkId,
+                in: {
+                    type_id: containTypeLinkId,
+                    from_id: triggeredByLinkId,
+                },
+            },
+            {
+                from_id: triggeredByLinkId,
+                type_id: usesApiKeyTypeLinkId,
+            },
+        ],
+    });
+    if (data.length === 0) {
+        throw new Error(`Link of type ##${apiKeyTypeLinkId} is not found`);
+    }
+    const usesLinks = data.filter(
+        (link) => link.type_id === usesApiKeyTypeLinkId
+    );
+    if (usesLinks.length > 1) {
+        throw new Error(
+            `More than 1 links of type ##${usesApiKeyTypeLinkId} are found`
+        );
+    }
+    const usesLink = data.find(
+        (link) => link.type_id === usesApiKeyTypeLinkId
+    );
+    if (usesLink) {
+        const tokenLink = data.find((link) => link.id === usesLink.to_id);
+        if (!tokenLink) {
+            throw new Error(`Link of type ##${apiKeyTypeLinkId} is not found`);
+        } else {
+            resultTokenLink = tokenLink;
+        }
+    } else {
+        const tokenLink = data.filter(
+            (link) => link.type_id === apiKeyTypeLinkId
+        );
+        if (tokenLink.length > 1) {
+            throw new Error(
+                `For 2 or more ##${apiKeyTypeLinkId} links you must activate it with usesOpenAiApiKey link`
+            );
+        } else {
+            const tokenLink = data.find(
+                (link) => link.type_id === apiKeyTypeLinkId
+            );
+            if (!tokenLink) {
+                throw new Error(`Link of type ##${apiKeyTypeLinkId} is not found`);
+            }
+            resultTokenLink = tokenLink;
+        }
+    }
+    if (!resultTokenLink.value?.value) {
+        throw new Error(`Link of type ##${apiKeyTypeLinkId} has no value`);
+    }
+    return resultTokenLink;
+}
+
+
   return response.data;
 }
