45319fba271d1b9e5faf149b824ffe92fb81ad38
FreePhoenix888 <freephoenix888@gmail.com>
2023-04-18 22:03:34 +0600

    Fix typo

--- imports/dialog/use-dialog-subscription.ts
+++ imports/dialog/use-dialog-subscription.ts
@@ -4,8 +4,8 @@ import { DIALOG_PACKAGE_NAME } from "./package-name";
 import { Link } from "@deep-foundation/deeplinks/imports/minilinks";
 import { notifyDialog } from "./notify-dialog";
 
-export function useDialogSubscription({deep, deviceLinkId, isEnaled = true}: {deep: DeepClient,deviceLinkId: number, isEnaled?: boolean}) {
-  if(!isEnaled) {
+export function useDialogSubscription({deep, deviceLinkId, isEnabled = true}: {deep: DeepClient,deviceLinkId: number, isEnabled?: boolean}) {
+  if(!isEnabled) {
     return;
   }
 
