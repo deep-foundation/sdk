f6b817edc58184ca27285f3869b80ebe04b5ac61
FreePhoenix888 <freephoenix888@gmail.com>
2023-04-19 21:03:27 +0600

    Add initDeviceIfNotInited

---
+++ imports/device/init-device-if-not-inited.ts
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { insertDevice } from "./insert-device";
+import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
+
+export async function initDeviceIfNotInited({deep, deviceLinkId, setDeviceLinkId} : {deep: DeepClient, deviceLinkId: number | undefined, setDeviceLinkId: (deviceLinkId: number | undefined) => void}) {
+   if(deviceLinkId) {
+      const { data: deviceLinks } = await deep.select(deviceLinkId);
+      if (deviceLinks.length === 0) {
+        setDeviceLinkId(undefined);
+        return {deviceLinkId: undefined}
+      }
+    } else {
+      const { deviceLink } = await insertDevice({ deep });
+      setDeviceLinkId(deviceLink.id);
+      return {deviceLinkId: deviceLink.id}
+    }
+}
