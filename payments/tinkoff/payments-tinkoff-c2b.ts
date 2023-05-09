b6785b54fced394d5cb0fb00e4b7864c4b180f9e
FreePhoenix888 <freephoenix888@gmail.com>
2023-05-09 16:12:35 +0000

    Install puppeteer and its dependencies at test start

--- payments/tinkoff/payments-tinkoff-c2b.ts
+++ payments/tinkoff/payments-tinkoff-c2b.ts
@@ -13,10 +13,12 @@ import { default as assert } from 'assert';
 import { createSerialOperation } from '@deep-foundation/deeplinks/imports/gql/serial.js';
 import path from 'path'
 import { sleep } from './sleep';
+import {execSync} from 'child_process'
 
 main();
 
 async function main() {
+  execSync(`npm install puppeteer && sudo apt update && sudo apt install -y libgbm-dev chromium-browser gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget`)
   const apolloClient = generateApolloClient({
     path: process.env.NEXT_PUBLIC_GQL_PATH || '', // <<= HERE PATH TO UPDATE
     ssl: !!~process.env.NEXT_PUBLIC_GQL_PATH.indexOf('localhost')
