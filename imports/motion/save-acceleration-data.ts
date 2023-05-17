dafdb6d92cfe0f01b6693c5cfeaf1374bad82bcb
FreePhoenix888 <freephoenix888@gmail.com>
2023-05-17 12:58:50 +0600

    Update saveAccelerationInfo

--- imports/motion/save-acceleration-data.ts
+++ imports/motion/save-acceleration-data.ts
@@ -3,21 +3,13 @@ import { PACKAGE_NAME } from "./package-name";
 import { BatteryInfo, Device, DeviceInfo, GetLanguageCodeResult, LanguageTag } from "@capacitor/device";
 import { Link } from "@deep-foundation/deeplinks/imports/minilinks";
 import { createSerialOperation } from "@deep-foundation/deeplinks/imports/gql";
-import { AccelListenerEvent } from "@capacitor/motion";
+import { AccelListenerEvent, Motion, RotationRate } from "@capacitor/motion";
 
-export async function saveAccelerationData(params: { deep: DeepClient, data: AccelListenerEvent } & ({deviceLinkId: number} | {deviceLink: Link<number>})) {
+export type AccelerationInfo = AccelListenerEvent | RotationRate;
+
+export async function saveAccelerationInfo(params: { deep: DeepClient, data: AccelListenerEvent | RotationRate } & ({deviceLinkId: number} | {deviceLink: Link<number>})) {
   const {deep} = params;
-  let deviceLink: Link<number>;
-  if('deviceLinkId' in params) {
-    const {data} = await deep.select({
-      id: params.deviceLinkId
-    })
-    deviceLink = data[0];
-  } else if('deviceLink' in params) {
-    deviceLink = params.deviceLink
-  } else {
-    throw new Error(`Either deviceLink or deviceLinkId must be passed`)
-  }
+  let deviceLink = await getDeviceLink();
   if(!deviceLink.value?.value) {
     await deep.serial({
       operations: [
@@ -46,5 +38,20 @@ export async function saveAccelerationData(params: { deep: DeepClient, data: Acc
         })
       ]
     })
+  }
+
+  async function getDeviceLink() {
+    let deviceLink: Link<number>;
+    if('deviceLinkId' in params) {
+      const {data} = await deep.select({
+        id: params.deviceLinkId
+      })
+      deviceLink = data[0];
+    } else if('deviceLink' in params) {
+      deviceLink = params.deviceLink
+    } else {
+      throw new Error(`Either deviceLink or deviceLinkId must be passed`)
+    }
+    return deviceLink;
   }
 }
