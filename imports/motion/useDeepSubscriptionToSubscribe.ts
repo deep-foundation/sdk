78292e03d2d0ef20d253097d57701d130543ee2e
FreePhoenix888 <freephoenix888@gmail.com>
2023-05-18 11:51:07 +0600

    Remove unused files

--- imports/motion/useDeepSubscriptionToSubscribe.ts
+++
@@ -1,9 +0,0 @@
-import { useDeepSubscription } from "@deep-foundation/deeplinks/imports/client";
-import { useEffect, useRef } from "react";
-import { MOTION_PACKAGE_NAME } from "./package-name";
-
-export function useDeepSubscriptionToSubscribeToAccelerationData({ deviceLinkId, subscribeCallback, unsubscribeCallback }: { deviceLinkId: number, subscribeCallback: () => void, unsubscribeCallback: () => void }) {
-
-
-
-}
