2d03553d65715ee6f00f1cd8688585ec9b85bd14
FreePhoenix888 <freephoenix888@gmail.com>
2023-05-18 11:59:43 +0600

    Add subscribeToOrientationChanges

---
+++ imports/motion/subscribe-to-orientation-changes.ts
@@ -0,0 +1,23 @@
+import { Motion } from '@capacitor/motion';
+import { saveMotionInfo } from './save-motion-info';
+import { DeepClient } from '@deep-foundation/deeplinks/imports/client';
+
+export async function subscribeToOrientationChanges({
+  deep,
+  deviceLinkId,
+}: SubscribeToOrientationChangesParam) {
+  Motion.addListener('orientation', async (rotationRate) => {
+    await saveMotionInfo({
+      deep,
+      deviceLinkId,
+      data: {
+        rotationRate,
+      },
+    });
+  });
+}
+
+export interface SubscribeToOrientationChangesParam {
+  deep: DeepClient;
+  deviceLinkId: number;
+}
