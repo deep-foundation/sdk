102ce36c0592b84ac40125f10e234af44c9147b3
FreePhoenix888 <freephoenix888@gmail.com>
2023-05-18 14:24:28 +0600

    Remove motion integration files

--- imports/motion/package-name.ts
+++
@@ -1,1 +0,0 @@
-export const MOTION_PACKAGE_NAME = "@deep-foundation/capacitor-motion";
--- imports/motion/request-motion-permissions.ts
+++
@@ -1,6 +0,0 @@
-export async function requestMotionPermissions() {
-  if ('requestPermission' in DeviceMotionEvent) {
-    // @ts-ignore
-    await DeviceMotionEvent.requestPermission();
-  }
-}
