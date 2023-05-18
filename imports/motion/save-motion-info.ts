a1d5a8bed43e6205075b14491959845db684f22d
FreePhoenix888 <freephoenix888@gmail.com>
2023-05-18 11:52:47 +0600

    Format

--- imports/motion/save-motion-info.ts
+++ imports/motion/save-motion-info.ts
@@ -1,4 +1,7 @@
-import { DeepClient, SerialOperation } from '@deep-foundation/deeplinks/imports/client';
+import {
+  DeepClient,
+  SerialOperation,
+} from '@deep-foundation/deeplinks/imports/client';
 import { MOTION_PACKAGE_NAME } from './package-name';
 import {
   BatteryInfo,
@@ -12,7 +15,9 @@ import { createSerialOperation } from '@deep-foundation/deeplinks/imports/gql';
 import { AccelListenerEvent, Motion, RotationRate } from '@capacitor/motion';
 import { BoolExpLink } from '@deep-foundation/deeplinks/imports/client_types';
 
-export type MotionInfo = AccelListenerEvent | Pick<AccelListenerEvent, 'rotationRate'>;
+export type MotionInfo =
+  | AccelListenerEvent
+  | Pick<AccelListenerEvent, 'rotationRate'>;
 
 export async function saveMotionInfo(
   params: { deep: DeepClient; data: MotionInfo } & (
@@ -21,31 +26,35 @@ export async function saveMotionInfo(
   )
 ) {
   const { deep } = params;
-  const serialOperations: Array<SerialOperation> = []
+  const serialOperations: Array<SerialOperation> = [];
   let deviceLink = await getDeviceLink();
   let motionLinkId: number;
   let value: MotionInfo | undefined;
-  const motionLink = await getMotionLinkOrUndefined({deviceLinkId: deviceLink.id});
-  if(motionLink) {
+  const motionLink = await getMotionLinkOrUndefined({
+    deviceLinkId: deviceLink.id,
+  });
+  if (motionLink) {
     motionLinkId = motionLink.id;
-    value = motionLink.value?.value
+    value = motionLink.value?.value;
   } else {
-    const reservedLinkIds = await deep.reserve(1)
-    motionLinkId = reservedLinkIds.pop()
+    const reservedLinkIds = await deep.reserve(1);
+    motionLinkId = reservedLinkIds.pop();
     serialOperations.push(
-      await getMotionLinkInsertSerialOperation({deviceLinkId: deviceLink.id, motionLinkId})
+      await getMotionLinkInsertSerialOperation({
+        deviceLinkId: deviceLink.id,
+        motionLinkId,
+      })
     );
   }
-  if(value) {
+  if (value) {
     serialOperations.push(
-      await getMotionLinkValueUpdateSerialOperation({motionLinkId})
+      await getMotionLinkValueUpdateSerialOperation({ motionLinkId })
     );
   } else {
     serialOperations.push(
-      await getMotionLinkValueInsertSerialOperation({motionLinkId})
+      await getMotionLinkValueInsertSerialOperation({ motionLinkId })
     );
   }
-  
 
   async function getDeviceLink() {
     let deviceLink: Link<number>;
@@ -62,35 +71,51 @@ export async function saveMotionInfo(
     return deviceLink;
   }
 
-  async function getMotionLinkOrUndefined({deviceLinkId}: {deviceLinkId: number}): Promise<Link<number> | undefined> {
-  const selectData: BoolExpLink = {
+  async function getMotionLinkOrUndefined({
+    deviceLinkId,
+  }: {
+    deviceLinkId: number;
+  }): Promise<Link<number> | undefined> {
+    const selectData: BoolExpLink = {
       type_id: {
-        _id: [MOTION_PACKAGE_NAME, "Motion"]
+        _id: [MOTION_PACKAGE_NAME, 'Motion'],
       },
       from_id: deviceLinkId,
-      to_id: deviceLinkId
-    }
-    const { data: [motionLink] } = await deep.select(selectData)
+      to_id: deviceLinkId,
+    };
+    const {
+      data: [motionLink],
+    } = await deep.select(selectData);
     // if (!motionLink) {
     //   throw new Error(`Select with data ${selectData} return empty result`)
     // }
-    return motionLink
+    return motionLink;
   }
 
-  async function getMotionLinkInsertSerialOperation({deviceLinkId, motionLinkId}: {deviceLinkId: number, motionLinkId: number}) {
+  async function getMotionLinkInsertSerialOperation({
+    deviceLinkId,
+    motionLinkId,
+  }: {
+    deviceLinkId: number;
+    motionLinkId: number;
+  }) {
     return createSerialOperation({
       table: 'links',
       type: 'insert',
       objects: {
         id: motionLinkId,
-        type_id: await deep.id(MOTION_PACKAGE_NAME, "Motion"),
+        type_id: await deep.id(MOTION_PACKAGE_NAME, 'Motion'),
         from_id: deviceLinkId,
-        to_id: deviceLinkId
-      }
-    })
+        to_id: deviceLinkId,
+      },
+    });
   }
 
-  async function getMotionLinkValueInsertSerialOperation({motionLinkId}: {motionLinkId: number}) {
+  async function getMotionLinkValueInsertSerialOperation({
+    motionLinkId,
+  }: {
+    motionLinkId: number;
+  }) {
     return createSerialOperation({
       table: 'objects',
       type: 'insert',
@@ -98,10 +123,14 @@ export async function saveMotionInfo(
         link_id: motionLinkId,
         value: params.data,
       },
-    })
+    });
   }
 
-  async function getMotionLinkValueUpdateSerialOperation({motionLinkId}: {motionLinkId: number}) {
+  async function getMotionLinkValueUpdateSerialOperation({
+    motionLinkId,
+  }: {
+    motionLinkId: number;
+  }) {
     return createSerialOperation({
       table: 'objects',
       type: 'update',
@@ -111,6 +140,6 @@ export async function saveMotionInfo(
       value: {
         value: { ...deviceLink.value.value, ...params.data },
       },
-    })
+    });
   }
 }
