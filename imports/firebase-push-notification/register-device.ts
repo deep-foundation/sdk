142b1bfd320fae14062c944aac0362d5fa6d19fd
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-22 19:54:46 +0600

    Fix double notification

--- imports/firebase-push-notification/register-device.ts
+++ imports/firebase-push-notification/register-device.ts
@@ -2,7 +2,7 @@ import { DeviceInfo } from '@capacitor/device';
 import { PushNotifications } from '@capacitor/push-notifications';
 import { DeepClient } from '@deep-foundation/deeplinks/imports/client';
 import { BoolExpLink } from '@deep-foundation/deeplinks/imports/client_types';
-import { getToken, Messaging } from 'firebase/messaging';
+import { getToken, Messaging, onMessage } from 'firebase/messaging';
 import { insertDeviceRegistrationToken } from './insert-device-registration-token';
 import { PACKAGE_NAME } from './package-name';
 
@@ -45,7 +45,7 @@ export async function registerDevice({
       { scope: 'firebase-cloud-messaging-push-scope' }
     );
 
-    const webPushCertificateLink = await getPushCertificateLink({deep})
+    const webPushCertificateLink = await getPushCertificateLink({ deep });
     const webPushCertificate = webPushCertificateLink.value.value;
 
     const deviceRegistrationToken = await getToken(firebaseMessaging, {
@@ -54,6 +54,34 @@ export async function registerDevice({
     });
 
     onDeviceRegistration({ value: deviceRegistrationToken });
+
+    onMessage(firebaseMessaging, async (payload) => {
+      console.log(
+        '[firebase-messaging-sw.js] Received background message ',
+        payload
+      );
+
+      // Let's check if the browser supports notifications
+      if (!('Notification' in window)) {
+        throw new Error('This browser does not support desktop notification');
+      }
+
+      // Let's check whether notification permissions have already been granted
+      else if (Notification.permission === 'granted') {
+        // If it's okay, let's create a notification
+        new Notification(payload.notification.title, payload.notification);
+      }
+
+      // Otherwise, we need to ask the user for permission
+      else if (Notification.permission !== 'denied') {
+        Notification.requestPermission().then(function (permission) {
+          // If the user accepts, let's create a notification
+          if (permission === 'granted') {
+            new Notification(payload.notification.title, payload.notification);
+          }
+        });
+      }
+    });
   } else {
     await PushNotifications.removeAllListeners();
     await PushNotifications.addListener('registration', onDeviceRegistration);
@@ -61,7 +89,7 @@ export async function registerDevice({
   }
 }
 
-async function getPushCertificateLink({deep}: {deep: DeepClient}) {
+async function getPushCertificateLink({ deep }: { deep: DeepClient }) {
   const webPushCertificateTypeLinkId = await deep.id(
     PACKAGE_NAME,
     'WebPushCertificate'
@@ -86,25 +114,43 @@ async function getPushCertificateLink({deep}: {deep: DeepClient}) {
       },
     ],
   };
-  const {
-    data,
-  } = await deep.select(selectData);
+  const { data } = await deep.select(selectData);
   let webPushCertificateLink;
-  if(data.length === 0) {
-    throw new Error(`Select with data ${JSON.stringify(selectData)} returned empty result`)
+  if (data.length === 0) {
+    throw new Error(
+      `Select with data ${JSON.stringify(selectData)} returned empty result`
+    );
   }
-  const usesWebPushCertificateLinks = data.filter(link => link.type_id === usesWebPushCertificateTypeLinkId)
-  if(usesWebPushCertificateLinks.length > 1) {
-    throw new Error(`Select with data ${JSON.stringify(selectData)} returned more than one link of type ##${usesWebPushCertificateTypeLinkId}: ${usesWebPushCertificateLinks.map(link => `##${link.id}`).join(',')}`)
+  const usesWebPushCertificateLinks = data.filter(
+    (link) => link.type_id === usesWebPushCertificateTypeLinkId
+  );
+  if (usesWebPushCertificateLinks.length > 1) {
+    throw new Error(
+      `Select with data ${JSON.stringify(
+        selectData
+      )} returned more than one link of type ##${usesWebPushCertificateTypeLinkId}: ${usesWebPushCertificateLinks
+        .map((link) => `##${link.id}`)
+        .join(',')}`
+    );
   } else if (usesWebPushCertificateLinks.length === 1) {
-    const usesWebPushCertificateLink = usesWebPushCertificateLinks[0]
-    webPushCertificateLink = data.find(link => link.id === usesWebPushCertificateLink.to_id)
+    const usesWebPushCertificateLink = usesWebPushCertificateLinks[0];
+    webPushCertificateLink = data.find(
+      (link) => link.id === usesWebPushCertificateLink.to_id
+    );
   } else {
-    const webPushCertificateLinks = data.filter(link => link.type_id === webPushCertificateTypeLinkId)
-    if(webPushCertificateLinks.length > 1) {
-      throw new Error(`Select with data ${JSON.stringify(selectData)} returned more than one link of type ##${webPushCertificateTypeLinkId}: ${webPushCertificateLinks.map(link => `##${link.id}`).join(',')}`)
+    const webPushCertificateLinks = data.filter(
+      (link) => link.type_id === webPushCertificateTypeLinkId
+    );
+    if (webPushCertificateLinks.length > 1) {
+      throw new Error(
+        `Select with data ${JSON.stringify(
+          selectData
+        )} returned more than one link of type ##${webPushCertificateTypeLinkId}: ${webPushCertificateLinks
+          .map((link) => `##${link.id}`)
+          .join(',')}`
+      );
     }
-    webPushCertificateLink = webPushCertificateLinks[0]
+    webPushCertificateLink = webPushCertificateLinks[0];
   }
   if (!webPushCertificateLink) {
     throw new Error(
@@ -115,5 +161,5 @@ async function getPushCertificateLink({deep}: {deep: DeepClient}) {
   if (!webPushCertificateLink.value?.value) {
     throw new Error(`##${webPushCertificateLink.id} must have a value`);
   }
-  return webPushCertificateLink
+  return webPushCertificateLink;
 }
