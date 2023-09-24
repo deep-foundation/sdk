307431a861d96556763641fd7c7b0e61f4f93586
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-24 16:19:10 +0600

    Revert "Use hooks"
    This reverts commit 6a4cc81af724bf4e0ce9b8235c3fa993e139e150.

--- imports/react/components/login.tsx
+++ imports/react/components/login.tsx
@@ -1,11 +1,9 @@
 import { Card, CardHeader, Heading, CardBody, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
 import { useState } from "react";
-import { useGqlPath } from "../hooks/use-gql-path";
-import { useToken } from "../hooks/use-token";
 
 export function Login(arg: { onSubmit: (arg: { gqlPath: string, token: string }) => void }) {
-  const [gqlPath, setGqlPath] = useGqlPath(undefined);
-  const [token, setToken] = useToken(undefined);
+  const [gqlPath, setGqlPath] = useState(undefined);
+  const [token, setToken] = useState(undefined);
   return <Card>
     <CardHeader>
       <Heading>
