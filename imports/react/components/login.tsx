6a4cc81af724bf4e0ce9b8235c3fa993e139e150
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-24 16:18:51 +0600

    Use hooks

--- imports/react/components/login.tsx
+++ imports/react/components/login.tsx
@@ -1,9 +1,11 @@
 import { Card, CardHeader, Heading, CardBody, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
 import { useState } from "react";
+import { useGqlPath } from "../hooks/use-gql-path";
+import { useToken } from "../hooks/use-token";
 
 export function Login(arg: { onSubmit: (arg: { gqlPath: string, token: string }) => void }) {
-  const [gqlPath, setGqlPath] = useState(undefined);
-  const [token, setToken] = useState(undefined);
+  const [gqlPath, setGqlPath] = useGqlPath(undefined);
+  const [token, setToken] = useToken(undefined);
   return <Card>
     <CardHeader>
       <Heading>
