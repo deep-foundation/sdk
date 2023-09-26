cece678f3dbe517b8682c219ad3a6eb98a2bf72b
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-26 15:14:27 +0600

    Remove unused code

--- imports/react/components/with-providers.tsx
+++ imports/react/components/with-providers.tsx
@@ -2,16 +2,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { DeepProvider } from "@deep-foundation/deeplinks/imports/client";
 import { TokenProvider } from "@deep-foundation/deeplinks/imports/react-token";
 import { ApolloClientTokenizedProvider } from "@deep-foundation/react-hasura/apollo-client-tokenized-provider";
-import { LocalStoreProvider } from "@deep-foundation/store/local";
-import { CapacitorStoreProvider } from "@deep-foundation/store/capacitor";
-import { CookiesStoreProvider } from "@deep-foundation/store/cookies";
-import { QueryStoreProvider } from "@deep-foundation/store/query";
-import { useGraphQlUrl } from "../hooks/use-gql-path";
-import { useDeepToken } from "../hooks/use-token";
-import { WithLogin } from "./with-login";
 import { StoreProvider } from "./store-provider";
 import { WithGraphQlUrl } from "./with-graphql-url";
-import { WithDeepToken } from "./with-deep-token";
 import { WithAddDebugFieldsToWindow } from "./with-add-debug-fields-to-window";
 
 export function WithProviders({ children }: { children: JSX.Element }) {
@@ -21,34 +13,26 @@ export function WithProviders({ children }: { children: JSX.Element }) {
         <TokenProvider>
           <WithGraphQlUrl
             renderChildren={({ graphQlUrl }) => (
-              <WithDeepToken
-                renderChildren={({ deepToken }) => {
-                  console.log({ graphQlUrl, deepToken });
-                  return (
-                    <ApolloClientTokenizedProvider
-                      options={{
-                        client: "@deep-foundation/sdk",
-                        ...(graphQlUrl && {
-                          path:
-                            new URL(graphQlUrl).host +
-                            new URL(graphQlUrl).pathname +
-                            new URL(graphQlUrl).search +
-                            new URL(graphQlUrl).hash,
-                          ssl: new URL(graphQlUrl).protocol === "https:",
-                        }),
-                        token: deepToken,
-                        ws: !!process?.browser,
-                      }}
-                    >
-                      <DeepProvider>
-                        <WithAddDebugFieldsToWindow>
-                          <WithLogin>{children}</WithLogin>
-                        </WithAddDebugFieldsToWindow>
-                      </DeepProvider>
-                    </ApolloClientTokenizedProvider>
-                  );
-                }}
-              />
+              <ApolloClientTokenizedProvider
+              options={{
+                client: "@deep-foundation/sdk",
+                ...(graphQlUrl && {
+                  path:
+                    new URL(graphQlUrl).host +
+                    new URL(graphQlUrl).pathname +
+                    new URL(graphQlUrl).search +
+                    new URL(graphQlUrl).hash,
+                  ssl: new URL(graphQlUrl).protocol === "https:",
+                }),
+                ws: !!process?.browser,
+              }}
+            >
+              <DeepProvider>
+                <WithAddDebugFieldsToWindow>
+                  {children}
+                </WithAddDebugFieldsToWindow>
+              </DeepProvider>
+            </ApolloClientTokenizedProvider>
             )}
           />
         </TokenProvider>
