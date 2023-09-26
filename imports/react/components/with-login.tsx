22fe15eb29eb02238229756f3f9d115dd7fbe15e
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-26 15:14:04 +0600

    Hotfix: rerender page if linkId is not changed

--- imports/react/components/with-login.tsx
+++ imports/react/components/with-login.tsx
@@ -8,28 +8,44 @@ import { processEnvs } from "../../process-envs";
 import { useDeepToken } from "../hooks/use-token";
 import { useGraphQlUrl } from "../hooks/use-gql-path";
 import { generateApolloClient } from "@deep-foundation/hasura/client";
-import { debug } from "../../debug";
+import { logPackage } from "../../logPackage";
+import debug from "debug";
+debug.enable("@deep-foundation/deep-memo-app:WithLogin");
 
 export function WithLogin({ children }: { children: JSX.Element }) {
-  const log = debug.extend(WithLogin.name);
-  const toast = useToast();
-  const [isAuthorized, setIsAuthorized] = useState(undefined);
+  const log = logPackage.extend(WithLogin.name);
   const [graphQlUrl, setGraphQlUrl] = useGraphQlUrl();
   const [deepToken, setDeepToken] = useDeepToken();
   const deep = useDeep();
+
+  const [isAuthorized, setIsAuthorized] = useState<boolean|undefined>(undefined);
+
+  useEffect(() => {
+    log({ graphQlUrl, deepToken, deep });
+    log({ deepLinkId: deep.linkId });
+    console.log("!deep.linkId && graphQlUrl && deepToken", Boolean(!deep.linkId && graphQlUrl && deepToken))
+    if(!deep.linkId && graphQlUrl && deepToken) {
+      window.location.reload();
+    } 
+    if(deep.linkId) {
+      setIsAuthorized(true);
+    }
+  }, [deep]);
+
+  useEffect(() => {
+    self["graphQlUrl"] = graphQlUrl;
+    self["setGraphQlUrl"] = setGraphQlUrl;
+    self["deepToken"] = deepToken;
+    self["setDeepToken"] = setDeepToken;
+    self["deep"] = deep;
+  });
 
   return isAuthorized ? (
     children
   ) : (
     <Login
       onSubmit={async (arg) => {
-        const gqlPathUrl = new URL(arg.gqlPath);
-        setGraphQlUrl(
-          gqlPathUrl.host +
-            gqlPathUrl.pathname +
-            gqlPathUrl.search +
-            gqlPathUrl.hash
-        );
+        setGraphQlUrl(arg.gqlPath);
         setDeepToken(arg.token);
       }}
     />
