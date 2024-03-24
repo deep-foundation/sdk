8c45d3baa70b91a00fdd10b52da6bdbde0250800
FreePhoenix888 <freephoenix888@gmail.com>
2024-03-24 11:00:48 +0500

    Fix types errro

--- src/react/hooks/use-gql-path.ts
+++ src/react/hooks/use-gql-path.ts
@@ -2,6 +2,6 @@ import { useLocalStore } from "@deep-foundation/store/local";
 import { processEnvs } from "../../process-envs";
 import { CapacitorStoreKeys } from "../../capacitor-store-keys";
 
-export function useGraphQlUrl(defaultValue: string = processEnvs.graphQlUrl) {
+export function useGraphQlUrl(defaultValue: string|undefined = processEnvs.graphQlUrl) {
   return useLocalStore(CapacitorStoreKeys[CapacitorStoreKeys.GraphQlUrl], defaultValue)
 }
