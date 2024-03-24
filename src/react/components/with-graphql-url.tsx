30320437b2b23166c4b70512d5e6a2d7fe7fe1a8
FreePhoenix888 <freephoenix888@gmail.com>
2024-03-24 11:01:54 +0500

    Fix ts types error

--- src/react/components/with-graphql-url.tsx
+++ src/react/components/with-graphql-url.tsx
@@ -9,5 +9,5 @@ export function WithGraphQlUrl(options: WithGraphQlUrlOptions) {
 }
 
 export interface WithGraphQlUrlOptions {
-  renderChildren: (param: { graphQlUrl: string, setGraphQlUrl: (newGraphQl: string) => void }) => JSX.Element;
+  renderChildren: (param: { graphQlUrl: string|undefined, setGraphQlUrl: (newGraphQl: string|undefined) => void }) => JSX.Element;
 }
