0fd20b6adb25b08531c19ee7961abfc6f66d69be
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-04-17 17:30:21 +0300

    disable noriaize for now

--- electron/notarize.cjs
+++ electron/notarize.cjs
@@ -4,23 +4,23 @@ const appBundleId = 'deep.app';
 
 exports.default = async function packageTask (context) {
   const { electronPlatformName, appOutDir } = context;
-  if (electronPlatformName !== "darwin") {
-    return;
-  }
-  const appName = context.packager.appInfo.productFilename;
-  console.log('Notarizing...');
-  console.log({
-    appBundleId,
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-  });
-  await notarize({
-    tool: 'notarytool',
-    appBundleId,
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-  });
-  console.log('Notarizing done');
+  // if (electronPlatformName !== "darwin") {
+  //   return;
+  // }
+  // const appName = context.packager.appInfo.productFilename;
+  // console.log('Notarizing...');
+  // console.log({
+  //   appBundleId,
+  //   appPath: `${appOutDir}/${appName}.app`,
+  //   appleId: process.env.APPLEID,
+  //   appleIdPassword: process.env.APPLEIDPASS,
+  // });
+  // await notarize({
+  //   tool: 'notarytool',
+  //   appBundleId,
+  //   appPath: `${appOutDir}/${appName}.app`,
+  //   appleId: process.env.APPLEID,
+  //   appleIdPassword: process.env.APPLEIDPASS,
+  // });
+  // console.log('Notarizing done');
 }
--- electron/src/server.ts
+++ electron/src/server.ts
@@ -1,7 +1,6 @@
 const express = require("express");
 const promisify = require("util").promisify;
-const exec = require("child_process").exec
-const engine = require("./engine")
+const exec = require("child_process").exec;
 
 const execP = promisify(exec);
 process.env['MIGRATIONS_HASURA_PATH'] = 'localhost:8080';
@@ -11,19 +10,10 @@ process.env['DEBUG'] = 'deeplinks:container-controller:*';
 const envsObj = { MIGRATIONS_HASURA_PATH: 'localhost:8080', MIGRATIONS_HASURA_SSL: '0', MIGRATIONS_HASURA_SECRET: 'myadminsecretkey' };
 
 (async () => {
-  const app = express();
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
-  app.post('/api/deeplinks', async (req, res) => {
-    res.json(await engine.call({ ...req.body, envs: { ...envsObj, ...req?.body?.envs } }));
-  });
-  app.post('/test', async (req, res) => {
-    res.json(await execP(`${req.body.exec}`));
-  });
-  app.post('/eval', async (req, res) => {
-    res.json({ eval: eval(req.body.eval) });
-  });
-  app.listen(3007, () => {
-    console.log(`Example app listening at http://localhost:3007`)
-  });
+  // const app = express();
+  // app.use(express.json())
+  // app.use(express.urlencoded({ extended: true }))
+  // app.listen(3007, () => {
+  //   console.log(`Example app listening at http://localhost:3007`)
+  // });
 })().catch(console.error);
