1e6cebcc5ff1d1cad44d03d199f3ae5d44c898af
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-04-19 20:05:25 +0300

    lang switch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import {
   Box,
+  Button,
   Center,
+  HStack,
   Heading,
   VStack
 } from '@chakra-ui/react';
 import { useDeep } from '@deep-foundation/deeplinks/imports/client';
 import { Connection } from '../src/connection';
 import { i18nGetStaticProps } from '../src/i18n';
+import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
 
 export default function Page() {
   const deep = useDeep();
+  const { t } = useTranslation();
+  const router = useRouter();
 
   // @ts-ignore
   if (typeof(window) === 'object') window.deep = deep;
@@ -18,8 +24,14 @@ export default function Page() {
   return (<Center p={'1em'}>
     <VStack p={3} spacing={3} width={'100vw'} maxWidth={500}>
       <Box pt={3}>
-        <Heading as={'h1'} size='xl'>Deep SDK</Heading>
-        <Heading as={'h4'} size='md'>Minimalistic template/boilerplate for any project.</Heading>
+        <Heading as={'h1'} size='xl'>
+          {t('sdk')}
+          <HStack spacing={3} float='right'>
+            <Button isDisabled={router.locale === 'ru'} onClick={() => router.push(router.asPath, router.asPath, { locale: 'ru' })}>ru</Button>
+            <Button isDisabled={router.locale === 'en'} onClick={() => router.push(router.asPath, router.asPath, { locale: 'en' })}>en</Button>
+          </HStack>
+        </Heading>
+        <Heading as={'h4'} size='md'>{t('sdk-description')}</Heading>
       </Box>
       <Connection/>
     </VStack>
