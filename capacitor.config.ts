c23252a2be0bf1df2bbf309e7a122701f2c0ac03
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-04-26 02:42:44 +0000

    app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.deepfoundation.deep',
   appName: 'Deep',
-  webDir: 'out',
+  webDir: 'app',
   bundledWebRuntime: false,
   plugins: {
     PushNotifications: {
