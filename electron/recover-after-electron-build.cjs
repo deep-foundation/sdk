0e1b09828c4be4e0842534c5af6d920ec66cfc05
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-04-26 11:40:49 +0300

    debug after electron build

--- electron/recover-after-electron-build.cjs
+++ electron/recover-after-electron-build.cjs
@@ -11,6 +11,18 @@ const macAppPath = 'dist/mac/sdk.app/Contents/Resources/app';
 const appPath = process.platform === 'win32' ? windowsAppPath : process.platform === 'darwin' ? macAppPath : linuxAppPath;
 
 exports.default = async function(context) {
+  if (process.platform === 'win32') {
+    try {
+      console.log('outDir', fs.readFileSync(`${context.outDir}`));
+      console.log('appOutDir', fs.readFileSync(`${context.appOutDir}`));
+      console.log('resources', fs.readFileSync(`${context.appOutDir}\resources`));
+      console.log('app', fs.readFileSync(`${context.appOutDir}\\resources\\app`));
+      console.log('build', fs.readFileSync(`${context.appOutDir}\\resources\\app\\build`));
+      console.log('src', fs.readFileSync(`${context.appOutDir}\\resources\\app\\build\\src`));
+    } catch(e) {
+      console.log('error', e);
+    }
+  }
   try {
     if (fs.existsSync(`${appPath}${delimetr}${typesPath}`))fs.rmSync(`${appPath}${delimetr}${typesPath}`, { recursive: true });
     shell(`cp -r ${typesPath} ${appPath}${delimetr}${typesPath}`);
