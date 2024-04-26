ffdd5f201828fa29d10c2ff6b7d71c2f9ec867c4
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-04-26 13:42:30 +0300

    fix releases

--- electron/recover-after-electron-build.cjs
+++ electron/recover-after-electron-build.cjs
@@ -11,26 +11,6 @@ const macAppPath = 'dist/mac/sdk.app/Contents/Resources/app';
 const appPath = process.platform === 'win32' ? windowsAppPath : process.platform === 'darwin' ? macAppPath : linuxAppPath;
 
 exports.default = async function(context) {
-  if (process.platform === 'win32') {
-    try {console.log('outDir\\..', `${context.outDir}\\..`);} catch(e) { console.log(e); }
-    try {console.log(fs.readdirSync(`${context.outDir}\\..`));} catch(e) { console.log(e); }
-    try {console.log('outDir\\..\\build', `${context.outDir}\\..\\build`);} catch(e) { console.log(e); }
-    try {console.log(fs.readdirSync(`${context.outDir}\\..\\build`));} catch(e) { console.log(e); }
-    try {console.log('outDir\\..\\build\\src', `${context.outDir}\\..\\build\\src`);} catch(e) { console.log(e); }
-    try {console.log(fs.readdirSync(`${context.outDir}\\..\\build\\src`));} catch(e) { console.log(e); }
-    try {console.log('outDir', `${context.outDir}`);} catch(e) { console.log(e); }
-    try {console.log(fs.readdirSync(`${context.outDir}`));} catch(e) { console.log(e); }
-    try {console.log('appOutDir', `${context.appOutDir}`);} catch(e) { console.log(e); }
-    try {console.log(fs.readdirSync(`${context.appOutDir}`));} catch(e) { console.log(e); }
-    try {console.log('resources', `${context.appOutDir}\\resources`);} catch(e) { console.log(e); }
-    try {console.log(fs.readdirSync(`${context.appOutDir}\\resources`));} catch(e) { console.log(e); }
-    try {console.log('app', `${context.appOutDir}\\resources\\app`);} catch(e) { console.log(e); }
-    try {console.log(fs.readdirSync(`${context.appOutDir}\\resources\\app`));} catch(e) { console.log(e); }
-    try {console.log('build', `${context.appOutDir}\\resources\\app\\build`);} catch(e) { console.log(e); }
-    try {console.log(fs.readdirSync(`${context.appOutDir}\\resources\\app\\build`));} catch(e) { console.log(e); }
-    try {console.log('src', `${context.appOutDir}\\resources\\app\\build\\src`);} catch(e) { console.log(e); }
-    try {console.log(fs.readdirSync(`${context.appOutDir}\\resources\\app\\build\\src`));} catch(e) { console.log(e); }
-  }
   try {
     if (fs.existsSync(`${appPath}${delimetr}${typesPath}`))fs.rmSync(`${appPath}${delimetr}${typesPath}`, { recursive: true });
     shell(`cp -r ${typesPath} ${appPath}${delimetr}${typesPath}`);
