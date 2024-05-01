7abb03128ce297cd8be9d09dfe658e231b4a5ec5
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-05-01 17:40:08 +0000

    server.js

---
+++ bin/server.js
@@ -0,0 +1,34 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next');
+const nextConfig = require('../next.config');
+
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = process.env.PORT || 3000;
+
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port, quiet: true, conf: nextConfig, dir: `${__dirname}/..` })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl)
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  })
+    .once('error', (err) => {
+      console.error(err)
+      process.exit(1)
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`)
+    })
+})
